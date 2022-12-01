1af360fdcd420f10410e2a6c3834f4d9d627892e
Chaudhuri, Anirban <anirbanc@oden.utexas.edu>
2022-12-01 04:09:23 +0000

    control codes

---
+++ riskOUU/CIEMSS/control/control.py
@@ -0,0 +1,23 @@
+######## Written by: Anirban Chaudhuri (Oden Institute; UT Austin)
+import numpy as np
+
+class Control():
+	def __init__(self):
+		self.name = "custom policy"
+		self.nu=None
+		self.beta=None
+		self.betaV=None
+
+	def vaccination_rate(self, rate=0.005):
+		# Vaccination rate controlled through parameter nu (/day)
+		self.name = "Vaccination rate: nu"        
+		self.nu = rate
+		return self
+
+	def masking_policy(self, rate=0.3):
+		# Masking policy rate controlled through parameters beta and betav
+		# TODO: fix how beta is assigned given masking policy rate
+		self.name = "Masking policy: beta"
+		self.beta = rate
+		self.betav = rate
+		return self
