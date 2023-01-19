c0937cc4da0c422d3acfbb651313bbeb67aec6af
Chaudhuri, Anirban <anirbanc@oden.utexas.edu>
2023-01-19 03:24:46 +0000

    Replace control.py

--- riskOUU/CIEMSS/control/control.py
+++ riskOUU/CIEMSS/control/control.py
@@ -21,3 +21,31 @@ class Control():
 		self.beta = rate
 		self.betav = rate
 		return self
+
+
+class Control_pyro():
+	def __init__(self):
+		self.name = "custom policy"
+		self.nu=None
+		self.beta=None
+		self.betaV=None
+
+	def vaccination_rate(self, rate=0.005):
+		# Vaccination rate controlled through parameter nu (/day)
+		self.name = "Vaccination rate parmeter: nu"        
+		self.nu = np.array([rate])
+		return self
+
+	def scheduled_vaccination(self, rate=[100.,50.,20.]):
+		# Vaccination rate controlled through parameter nu (/day)
+		self.name = "Vaccination rate: svlux"        
+		self.svflux = np.array([rate])
+		return self
+
+	def masking_policy(self, rate=0.3):
+		# Masking policy rate controlled through parameters beta and betav
+		# TODO: fix how beta is assigned given masking policy rate
+		self.name = "Masking policy: beta"
+		self.beta = rate
+		self.betav = rate
+		return self
