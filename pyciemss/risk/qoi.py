2f9dc63c745207171e6617e6c1ad4241df3d28ed
Graham Pash <gtpash@utexas.edu>
2023-01-24 14:27:24 -0500

    remove non abstract form

--- pyciemss/risk/qoi.py
+++ pyciemss/risk/qoi.py
@@ -7,19 +7,3 @@ def nday_rolling_average(samples: np.ndarray, tf=90., ndays=7, dt=1.) -> np.ndar
     '''
     samples_ndays = samples[int(tf/dt)-ndays+1:int(tf/dt)+1, :]
     return np.mean(samples_ndays, axis=0)
-
-# TODO: make generic rolling average
-def rolling_average_infected(I: np.ndarray, Iv=None, tf=90, ndays=7, dt=1.) -> np.ndarray:
-    '''
-    Return estimate of n-day average of total infections.
-    I is a numpy array of shape (num_samples, num_time_steps).
-    Iv is optionally a numpy array of shape (num_samples, num_time_steps).
-    '''
-    # Estimate n-day average of cases
-    if Iv is None:
-        I_ndays = I[int(tf/dt)-ndays+1:int(tf/dt)+1, :]
-        return np.mean(I_ndays, axis=0)
-    else:
-        I_ndays = I[int(tf/dt)-ndays+1:int(tf/dt)+1, :]
-        Iv_ndays = Iv[int(tf/dt)-ndays+1:int(tf/dt)+1, :]
-        return np.mean(I_ndays + Iv_ndays, axis=0)
