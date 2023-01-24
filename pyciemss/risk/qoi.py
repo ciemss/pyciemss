e13d013557d6502f64f162250dd4a2ba6be9486d
Graham Pash <gtpash@utexas.edu>
2023-01-24 09:39:33 -0500

    add general nday rolling average

--- pyciemss/risk/qoi.py
+++ pyciemss/risk/qoi.py
@@ -1,5 +1,14 @@
 import numpy as np
 
+def nday_rolling_average(samples: np.ndarray, tf=90., ndays=7, dt=1.) -> np.ndarray:
+    '''
+    Return estimate of n-day average of samples.
+    samples is a numpy array of shpae (num_samples, num_times_steps).
+    '''
+    samples_ndays = samples[int(tf/dt)-ndays+1:int(tf/dt)+1, :]
+    return np.mean(samples_ndays, axis=0)
+
+# TODO: make generic rolling average
 def rolling_average_infected(I: np.ndarray, Iv=None, tf=90, ndays=7, dt=1.) -> np.ndarray:
     '''
     Return estimate of n-day average of total infections.
@@ -8,9 +17,9 @@ def rolling_average_infected(I: np.ndarray, Iv=None, tf=90, ndays=7, dt=1.) -> n
     '''
     # Estimate n-day average of cases
     if Iv is None:
-        I_ndays = I[:, int(tf/dt)-ndays+1:int(tf/dt)+1]
-        return np.mean(I_ndays, axis=1)
+        I_ndays = I[int(tf/dt)-ndays+1:int(tf/dt)+1, :]
+        return np.mean(I_ndays, axis=0)
     else:
-        I_ndays = I[:, int(tf/dt)-ndays+1:int(tf/dt)+1]
-        Iv_ndays = Iv[:, int(tf/dt)-ndays+1:int(tf/dt)+1]
-        return np.mean(I_ndays + Iv_ndays, axis=1)
+        I_ndays = I[int(tf/dt)-ndays+1:int(tf/dt)+1, :]
+        Iv_ndays = Iv[int(tf/dt)-ndays+1:int(tf/dt)+1, :]
+        return np.mean(I_ndays + Iv_ndays, axis=0)
