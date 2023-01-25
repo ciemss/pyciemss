98e243317bba2c6acc01fe4c1d1e0cb6fa4432a5
Graham Pash <gtpash96@gmail.com>
2023-01-25 16:51:30 -0500

    pseudocode stubs for qois for demos

--- src/pyciemss/risk/qoi.py
+++ src/pyciemss/risk/qoi.py
@@ -7,3 +7,16 @@ def nday_rolling_average(samples: np.ndarray, tf=90., ndays=7, dt=1.) -> np.ndar
     '''
     samples_ndays = samples[int(tf/dt)-ndays+1:int(tf/dt)+1, :]
     return np.mean(samples_ndays, axis=0)
+
+
+# TODO: rewrite this so it's not pseudocode
+def fraction_infected(samples: np.ndarray) -> np.ndarray:
+    return np.elementwise_division(samples["I_obs"], samples["N"])
+
+
+# TODO: rewrite this so it's not pseudocode
+def exceedence_threshold(samples, threshold):
+    if np.any(samples < threshold):
+        return 1
+    else:
+        return 0
