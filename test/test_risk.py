cc026a65c84505edb226a20d4aadb7ab17562fff
Graham Pash <gtpash96@gmail.com>
2023-01-26 21:46:02 -0500

    add and check risk measure tests

--- test/test_risk.py
+++ test/test_risk.py
@@ -3,11 +3,11 @@ import unittest
 from pyciemss.risk.risk_measures import mean
 import numpy as np
 
-class TestRiskMeasures(unittest.TestCase):
+class TestRisk(unittest.TestCase):
     ''''
     Tests for risk measures.
     '''
     def test_mean(self):
-        actual = mean(np.array(0, 1, 2))
+        actual = mean(np.array([0, 1, 2]))
         expected = 1
         self.assertEqual(actual, expected)
