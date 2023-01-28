9e832139fe307d389b546029a8d2238b3548c4d9
Joseph Cottam <joseph.cottam@pnnl.gov>
2023-01-28 17:46:27 -0500

    Workflow checks and related test data

---
+++ src/pyciemss/workflow/checks.py
@@ -0,0 +1,87 @@
+import pyciemss.workflow.vega as vega
+import numpy as np
+
+def contains(ref_lower, ref_upper, pct=None):
+    """Check-generator function. Returns a function that performs a test.
+    
+    returns -- A functiont that takes a list of bins and tests the lower/upper bound against those bins.
+               The signature of the returned function is (List[Number]) -> bool.
+               
+               If pct IS NOT SUPPLIED to the generator function, the returned function checks 
+               if ref_lower and ref_upper are within the distribution range.
+               
+               If pct IS SUPPLIED, teh returned function checks that the precent of 
+               distribution between ref_lower and ref_upper is AT LEAST pct% of the total data.
+    """
+    def pct_test(bins):
+        total = bins["count"].sum()
+        covered = bins[(bins["bin0"]>=ref_lower) & (bins["bin1"] <= ref_upper)]["count"].sum()
+
+        return  covered/total >= pct
+    
+    def simple_test(bins):
+        data_lower = min(bins["bin0"].min(), bins["bin1"].min())
+        data_upper = max(bins["bin0"].max(), bins["bin1"].max())
+        return ((data_lower <= ref_lower <= data_upper)
+                and (data_lower <= ref_upper <= data_upper))
+
+    if pct is not None:
+        return pct_test
+    else:
+        return simple_test
+    
+
+def KL(max_acceptable):
+    """Check-generator function. Returns a function that performs a test.
+    
+    max_acceptable -- Threshold for the returned check
+    returns -- Returns a function that checks if KL/divergence of two lists of bin-counts is less than max_acceptable.
+               Returned function takes two lists of bins-counts and returns a boolean result.  The signature
+               is roughly (list[Number], list[Number]) -> bool.    
+    """
+    def KL(a, b):
+        a = np.asarray(a, dtype=np.float32)+1
+        b = np.asarray(b, dtype=np.float32)+1
+
+        return np.sum(np.where(a != 0, a * np.log(a / b), 0)) <= max_acceptable
+    return KL
+    
+    
+        
+def prior_predictive(data, lower, upper, *, tests=[], combiner=all, **kwargs):
+    bins, schema = vega.histogram_static(data, xref=[lower, upper], 
+                                         return_bins=True, **kwargs)
+    checks = [test(bins) for test in tests]
+    
+    status = combiner(checks)
+    if not status:
+        status = f"Failed ({sum(checks)/len(checks):.2%} passing)"
+    else:
+        status = "Passed"
+    
+    schema["title"]["text"] = ["Prior Predictive Test (Histogram)", status]
+    
+    return checks, schema
+
+
+def posterior_predictive(posterior, data,  *, tests=[], combiner=all, **kwargs):
+    bins, schema = vega.histogram_multi(Posterior=posterior, Reference=data,
+                                  return_bins=True, **kwargs)
+    
+    groups = dict([*bins.groupby("label")])
+    posterior_dist = groups["Posterior"]["count"].values
+    reference_dist = groups["Reference"]["count"].values
+    
+    checks = [test(reference_dist, posterior_dist) for test in tests]
+    status = combiner(checks)
+    
+    if not status:
+        status = f"Failed ({sum(checks)/len(checks):.2%} passing)"
+    else:
+        status = "Passed"
+    
+    schema["title"]["text"] = ["Posterior Predictive Check (Histogram)", status]
+    
+    return checks, schema
+    
+    
---
+++ src/pyciemss/workflow/vega.py
@@ -0,0 +1,123 @@
+from copy import deepcopy
+import IPython.display
+import pandas as pd
+import numpy as np
+
+import importlib
+import json
+
+_resource_root = importlib.resources.files("pyciemss.workflow")
+histogram_static_schema = _resource_root.joinpath("histogram_static_bins.vg.json")
+histogram_multi_schema = _resource_root.joinpath("histogram_static_bins_multi.vg.json")
+
+def histogram_static(data, *, 
+                     xref=[],
+                     yref=[],
+                     bins=50, 
+                     return_bins=False):
+    """
+    Create a histogram with server-side binning.
+    
+    data - Data to plot
+    xref - List of values in the bin-range to highlight as vertical lines 
+    yref - List of values in the count-range to highlight as horizontal lines 
+    bins - Number of bins to divide into
+    """
+    subset = data["state_values"]
+    counts, edges = np.histogram(subset, bins=bins)
+    spans = [*(zip(edges, edges[1:]))]
+    desc = [{"bin0": l.item(), "bin1": h.item(), "count": c.item()} 
+            for ((l, h), c) in zip(spans, counts)]
+    
+    with open(histogram_static_schema) as f:
+        schema = json.load(f)
+
+    #TODO: This index-based setting seems fragine beyond belief!  I would like to do it as
+    # 'search this dict-of-dicts and replace the innder-dict that has name-key Y'
+    schema["data"][0] = {"name": "binned", "values": desc}
+    schema["data"][1] = {"name": "xref", 
+                         "values": [{"value": v} for v in xref]}
+    schema["data"][2] = {"name": "yref", 
+                         "values": [{"count": v} for v in yref]}
+    
+    if return_bins:
+        return pd.DataFrame(desc), schema
+    else: 
+        return schema
+    
+    
+def histogram_multi(*, xref=[],
+                     yref=[],
+                     bins=50, 
+                     return_bins=False,
+                   **data):
+    """
+    Create a histogram with server-side binning.
+    
+    TODO: Maybe compute overlap in bins explicitly and visualize as stacked?
+          Limits (practically) to two distributions, but legend is more clear.
+    
+    data - Data to plot
+    xref - List of values in the bin-range to highlight as vertical lines 
+    yref - List of values in the count-range to highlight as horizontal lines 
+    bins - Number of bins to divide into
+    """
+    def hist(label, subset):
+        subset = subset["state_values"]
+        counts, edges = np.histogram(subset, bins=bins)
+        spans = [*(zip(edges, edges[1:]))]
+        desc = [{"bin0": l.item(), "bin1": h.item(), "count": c.item(), "label": label} 
+                for ((l, h), c) in zip(spans, counts)]
+        return desc
+    
+    hists = [hist(label, group) for label, group in data.items()]
+    desc = [item for sublist in hists
+             for item in sublist]
+    
+    with open (histogram_multi_schema) as f:
+        schema = json.load(f)
+
+    #TODO: This index-based setting seems fragine beyond belief!  I would like to do it as
+    # 'search this dict-of-dicts and replace the innder-dict that has name-key Y'
+    schema["data"][0] = {"name": "binned", "values": desc}
+    schema["data"][1] = {"name": "xref", 
+                         "values": [{"value": v} for v in xref]}
+    schema["data"][2] = {"name": "yref", 
+                         "values": [{"count": v} for v in yref]}
+    
+    if return_bins:
+        return pd.DataFrame(desc), schema
+    else: 
+        return schema    
+    
+    
+    
+#### --- Utlity functions for working with Vega ----
+    
+
+def ipy_display(spec, *, lite=False):
+    """Wrap for dispaly in an ipython notebook.
+    spec -- A vega JSON schema ready for rendering
+    """
+    if lite:
+        bundle = {'application/vnd.vegalite.v5+json': spec}
+    else:
+        bundle = {'application/vnd.vega.v5+json': spec}
+
+    return IPython.display.display(bundle, raw=True)
+
+def resize(schema, *, w=None, h=None):
+    """Utility for changing the size of a schema. 
+    Always returns a copy of the original schema.
+    
+    schema -- Schema to change
+    w -- New width (optional)
+    h -- New height (optional)
+    """
+    schema = deepcopy(schema)
+    
+    if h is not None: schema["height"] = h
+    if w is not None: schema["width"] = w
+
+    return schema
+    
