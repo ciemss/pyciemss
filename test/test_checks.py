7be407a4581a84cb9eb7dbbe4d7f128904335934
Joseph Cottam <joseph.cottam@pnnl.gov>
2023-01-29 00:54:32 -0800

    Stubs for check functions

---
+++ test/test_checks.py
@@ -0,0 +1,19 @@
+from pyciemss.workflow import checks
+import unittest
+
+class TestCheck(unittest.TestCase):
+    def test_KL(self):
+        pass
+    
+    def test_contains(self):
+        pass
+
+    def test_contains_pct(self):
+        pass
+    
+    def test_prior_predictive(self):
+        pass
+
+    def test_posterior_predictive(self):
+        pass
+    
