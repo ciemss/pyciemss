1a3459797f99c099c45ea046a58c6efe9c93b069
Joseph Cottam <joseph.cottam@pnnl.gov>
2023-01-30 17:22:58 -0800

    Types and flake8 (partial) compliance

--- src/pyciemss/workflow/vega.py
+++ src/pyciemss/workflow/vega.py
@@ -1,3 +1,6 @@
+from typing import List, Dict, Any, Callable
+from numbers import Number
+
 from copy import deepcopy
 import IPython.display
 import pandas as pd
@@ -6,48 +9,50 @@ import numpy as np
 import pkgutil
 import json
 
+
 def _histogram_multi_schema():
     return json.loads(pkgutil.get_data(__name__, "histogram_static_bins_multi.vg.json"))
 
 
 def sturges_bin(data):
-    """Determine number of bin susing sturge's rule.  
-    TODO: Consider a Freedman-Diaconis rule (larger data sizes, large spread in data...)
+    """Determine number of bin susing sturge's rule.
+    TODO: Consider Freedman-Diaconis (larger data sizes and spreads)
     """
     return int(np.ceil(np.log2(len(data))) + 1)
 
 
-def histogram_multi(*, xrefs=[],
-                     yrefs=[], 
-                     bin_rule=sturges_bin,
-                     return_bins=False,
-                   **data):
+def histogram_multi(*, xrefs: List[Number] = [],
+                    yrefs: List[Number] = [],
+                    bin_rule: Callable = sturges_bin,
+                    return_bins: bool = False,
+                    **data):
     """
     Create a histogram with server-side binning.
 
-    
     TODO: Maybe compute overlap in bins explicitly and visualize as stacked?
           Limits (practically) to two distributions, but legend is more clear.
-    
-    TODO: Need to align histogram bin size between groups to make the visual representation more interpretable
-    
-    **data -- Keyword-tagged datasets.  Key will become a label.
-    bin_rule -- Determines bins width using this function. Will received a joint dataframe of all data passed
-    xrefs - List of values in the bin-range to highlight as vertical lines 
-    yrefs - List of values in the count-range to highlight as horizontal lines 
+
+    TODO: Need to align histogram bin size between groups to make the visual
+          representation more interpretable
+
+    **data -- Datasets, name will be used to build labels.
+    bin_rule -- Determines bins width using this function.
+                Will received a joint dataframe of all data passed
+    xrefs - List of values in the bin-range to highlight as vertical lines
+    yrefs - List of values in the count-range to highlight as horizontal lines
     bins - Number of bins to divide into
     """
-    
+
     schema = _histogram_multi_schema()
-    
+
     def hist(label, subset, edges):
         assignments = np.digitize(subset, edges)-1
         counts = np.bincount(assignments)
         spans = [*(zip(edges, edges[1:]))]
-        desc = [{"bin0": l.item(), "bin1": h.item(), "count": c.item(), "label": label} 
+        desc = [{"bin0": l.item(), "bin1": h.item(), "count": c.item(), "label": label}
                 for ((l, h), c) in zip(spans, counts)]
         return desc
-    
+
     def as_value_list(label, data):
         try:
             return data["state_values"].rename(label)
@@ -62,28 +67,24 @@ def histogram_multi(*, xrefs=[],
 
     hists = {k: hist(k, subset, edges) for k, subset in data.items()}
     desc = [item for sublist in hists.values()
-             for item in sublist]
-    
+            for item in sublist]
 
-    #TODO: This index-based setting seems fragine beyond belief!  I would like to do it as
+    # TODO: This index-based setting seems fragine beyond belief!  I would like to do it as
     # 'search this dict-of-dicts and replace the innder-dict that has name-key Y'
     schema["data"][0] = {"name": "binned", "values": desc}
-    schema["data"][1] = {"name": "xref", 
+    schema["data"][1] = {"name": "xref",
                          "values": [{"value": v} for v in xrefs]}
-    schema["data"][2] = {"name": "yref", 
+    schema["data"][2] = {"name": "yref",
                          "values": [{"count": v} for v in yrefs]}
-    
+
     if return_bins:
         return schema, pd.DataFrame(desc)
-    else: 
-        return schema    
-    
-    
-    
-#### --- Utlity functions for working with Vega ----
-    
+    else:
+        return schema
 
-def ipy_display(spec, *, lite=False):
+
+# --- Utlity functions for working with Vega ----
+def ipy_display(spec:  Dict[str, Any], *, lite=False):
     """Wrap for dispaly in an ipython notebook.
     spec -- A vega JSON schema ready for rendering
     """
@@ -94,18 +95,20 @@ def ipy_display(spec, *, lite=False):
 
     return IPython.display.display(bundle, raw=True)
 
-def resize(schema, *, w=None, h=None):
-    """Utility for changing the size of a schema. 
+
+def resize(schema: Dict[str, Any], *, w: int = None, h: int = None):
+    """Utility for changing the size of a schema.
     Always returns a copy of the original schema.
-    
+
     schema -- Schema to change
     w -- New width (optional)
     h -- New height (optional)
     """
     schema = deepcopy(schema)
-    
-    if h is not None: schema["height"] = h
-    if w is not None: schema["width"] = w
+
+    if h is not None:
+        schema["height"] = h
+    if w is not None:
+        schema["width"] = w
 
     return schema
-    
