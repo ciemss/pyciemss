267e8e2d42596066d026e3a5869c571ecc43b7b3
Sam Witty <samawitty@gmail.com>
2023-03-13 10:02:27 -0400

    added placeholder MeasurementEvent

--- src/pyciemss/ODE/events.py
+++ src/pyciemss/ODE/events.py
@@ -20,7 +20,16 @@ class StaticStopEvent(Event):
 
     def forward(self, t: Tensor, state: tuple[Tensor, ...]) -> Tensor:
         return t - self.time
-    
+
+class MeasurementEvent(Event):
+    # Use this event type to measure the state of the system at a given time.
+    # This will not trigger an observation, nor will it stop the trajectory.
+    def __init__(self, time: Tensor):
+        self.time = time
+
+    def forward(self, t: Tensor, state: tuple[Tensor, ...]) -> Tensor:
+        return t - self.time
+
 class DynamicStopEvent(Event):
     def forward(self, t: Tensor, state: tuple[Tensor, ...]) -> Tensor:
         raise NotImplementedError
