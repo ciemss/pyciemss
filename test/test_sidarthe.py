250f06c76d8df0a92867867701d9dcbf93672e1d
Sam Witty <samawitty@gmail.com>
2023-04-06 11:52:18 -0400

    Revert "Scenario 2 notebooks with risk-based OUU" (#96)

--- test/test_sidarthe.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from pyciemss.ODE.models import SIDARTHE
-
-class SIDARTHETest(unittest.TestCase):
-    def test_initialize(self):
-        self.SIDARTHE = SIDARTHE()
-        self.assertIsNotNone(self.SIDARTHE)
