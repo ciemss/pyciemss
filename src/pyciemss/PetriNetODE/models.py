1e33ccb40899e83aacfd986b9837c988589f7ed7
Jeremy Zucker <Jeremy.Zucker@pnnl.gov>
2023-04-15 23:05:40 -0400

    Documented MIRA_I_obs_with_scaled_Gaussian_noise and ran black on models.py

--- src/pyciemss/PetriNetODE/models.py
+++ src/pyciemss/PetriNetODE/models.py
@@ -6,29 +6,39 @@ import pyro.distributions as dist
 
 from pyro.nn import pyro_method
 import mira
-from pyciemss.PetriNetODE.base import ScaledBetaNoisePetriNetODESystem, MiraPetriNetODESystem, PetriNetODESystem, Time, State, Solution, get_name
+from pyciemss.PetriNetODE.base import (
+    ScaledBetaNoisePetriNetODESystem,
+    MiraPetriNetODESystem,
+    PetriNetODESystem,
+    Time,
+    State,
+    Solution,
+    get_name,
+)
 from pyciemss.utils import state_flux_constraint
 from pyciemss.utils.distributions import ScaledBeta
 
+
 class SVIIvR(PetriNetODESystem):
-    def __init__(self,
-                N,
-                noise_prior=dist.Uniform(5., 10.),
-                beta_prior=dist.Uniform(0.1, 0.3),
-                betaV_prior=dist.Uniform(0.025, 0.05),
-                gamma_prior=dist.Uniform(0.05, 0.35),
-                gammaV_prior=dist.Uniform(0.1, 0.4),
-                nu_prior=dist.Uniform(0.001, 0.01)
-                ):
+    def __init__(
+        self,
+        N,
+        noise_prior=dist.Uniform(5.0, 10.0),
+        beta_prior=dist.Uniform(0.1, 0.3),
+        betaV_prior=dist.Uniform(0.025, 0.05),
+        gamma_prior=dist.Uniform(0.05, 0.35),
+        gammaV_prior=dist.Uniform(0.1, 0.4),
+        nu_prior=dist.Uniform(0.001, 0.01),
+    ):
         super().__init__()
 
         self.N = N
-        self.noise_prior  = noise_prior
-        self.beta_prior   = beta_prior
-        self.betaV_prior  = betaV_prior
-        self.gamma_prior  = gamma_prior
+        self.noise_prior = noise_prior
+        self.beta_prior = beta_prior
+        self.betaV_prior = betaV_prior
+        self.gamma_prior = gamma_prior
         self.gammaV_prior = gammaV_prior
-        self.nu_prior     = nu_prior
+        self.nu_prior = nu_prior
 
     @pyro_method
     def deriv(self, t: Time, state: State) -> State:
@@ -36,25 +46,29 @@ class SVIIvR(PetriNetODESystem):
 
         # Local fluxes exposed to pyro for interventions.
         # Note: This only works with solvers that use fixed time increments, such as Euler's method. Otherwise, we have name collisions.
-        SV_flux_  = pyro.deterministic("SV_flux %f" % (t),  self.nu * S)
-        SI_flux_  = pyro.deterministic("SI_flux %f" % (t),  self.beta  * S * (I + Iv) / self.N)
-        VIv_flux_ = pyro.deterministic("VIv_flux %f" % (t), self.betaV * V * (I + Iv) / self.N)
-        IR_flux_  = pyro.deterministic("IR_flux %f" % (t),  self.gamma * I)
+        SV_flux_ = pyro.deterministic("SV_flux %f" % (t), self.nu * S)
+        SI_flux_ = pyro.deterministic(
+            "SI_flux %f" % (t), self.beta * S * (I + Iv) / self.N
+        )
+        VIv_flux_ = pyro.deterministic(
+            "VIv_flux %f" % (t), self.betaV * V * (I + Iv) / self.N
+        )
+        IR_flux_ = pyro.deterministic("IR_flux %f" % (t), self.gamma * I)
         IvR_flux_ = pyro.deterministic("IvR_flux %f" % (t), self.gammaV * Iv)
 
         # these state_flux_constraints ensure that we don't have vaccinated people become susceptible, etc.
-        SV_flux = state_flux_constraint(S,  SV_flux_)
-        SI_flux = state_flux_constraint(S,  SI_flux_)
-        VIv_flux = state_flux_constraint(V,  VIv_flux_)
+        SV_flux = state_flux_constraint(S, SV_flux_)
+        SI_flux = state_flux_constraint(S, SI_flux_)
+        VIv_flux = state_flux_constraint(V, VIv_flux_)
         IR_flux = state_flux_constraint(I, IR_flux_)
         IvR_flux = state_flux_constraint(Iv, IvR_flux_)
 
         # Where the real magic happens.
-        dSdt  = -SI_flux - SV_flux
-        dVdt  = -VIv_flux + SV_flux
-        dIdt  = SI_flux - IR_flux
+        dSdt = -SI_flux - SV_flux
+        dVdt = -VIv_flux + SV_flux
+        dIdt = SI_flux - IR_flux
         dIvdt = VIv_flux - IvR_flux
-        dRdt  = IR_flux + IvR_flux
+        dRdt = IR_flux + IvR_flux
 
         return dSdt, dVdt, dIdt, dIvdt, dRdt
 
@@ -62,14 +76,16 @@ class SVIIvR(PetriNetODESystem):
     def param_prior(self) -> None:
 
         self.noise_var = pyro.sample("noise_var", self.noise_prior)
-        self.beta      = pyro.sample("beta", self.beta_prior)
-        self.betaV     = pyro.sample("betaV", self.betaV_prior)
-        self.gamma     = pyro.sample("gamma", self.gamma_prior)
-        self.gammaV    = pyro.sample("gammaV", self.gammaV_prior)
-        self.nu        = pyro.sample("nu", self.nu_prior)
+        self.beta = pyro.sample("beta", self.beta_prior)
+        self.betaV = pyro.sample("betaV", self.betaV_prior)
+        self.gamma = pyro.sample("gamma", self.gamma_prior)
+        self.gammaV = pyro.sample("gammaV", self.gammaV_prior)
+        self.nu = pyro.sample("nu", self.nu_prior)
 
     @pyro_method
-    def observation_model(self, solution: Solution, data: Optional[Dict[str, State]] = None) -> Solution:
+    def observation_model(
+        self, solution: Solution, data: Optional[Dict[str, State]] = None
+    ) -> Solution:
         S, V, I, Iv, R = solution
 
         # It's a little clunky that we have to do `None` handling for each implementation of 'observation_model'...
@@ -78,11 +94,19 @@ class SVIIvR(PetriNetODESystem):
 
         # TODO: Make sure observations are strictly greater than 0.
 
-        S_obs = pyro.sample("S_obs", dist.Normal(S, self.noise_var).to_event(1), obs=data["S_obs"])
-        V_obs = pyro.sample("V_obs", dist.Normal(V, self.noise_var).to_event(1), obs=data["V_obs"])
+        S_obs = pyro.sample(
+            "S_obs", dist.Normal(S, self.noise_var).to_event(1), obs=data["S_obs"]
+        )
+        V_obs = pyro.sample(
+            "V_obs", dist.Normal(V, self.noise_var).to_event(1), obs=data["V_obs"]
+        )
         # We only observe the total number of infected people we don't know which of them are vaccinated.
-        I_obs = pyro.sample("I_obs", dist.Normal(I + Iv, self.noise_var).to_event(1), obs=data["I_obs"])
-        R_obs = pyro.sample("R_obs", dist.Normal(R, self.noise_var).to_event(1), obs=data["R_obs"])
+        I_obs = pyro.sample(
+            "I_obs", dist.Normal(I + Iv, self.noise_var).to_event(1), obs=data["I_obs"]
+        )
+        R_obs = pyro.sample(
+            "R_obs", dist.Normal(R, self.noise_var).to_event(1), obs=data["R_obs"]
+        )
 
         return (S_obs, V_obs, I_obs, R_obs)
 
@@ -95,14 +119,26 @@ class MIRA_SVIIvR(ScaledBetaNoisePetriNetODESystem):
             mean = solution["I"] + solution["I_v"]
         else:
             mean = solution[var_name]
-        pyro.sample(var_name, ScaledBeta(mean, self.total_population, self.pseudocount*mean).to_event(1))
+        pyro.sample(
+            var_name,
+            ScaledBeta(mean, self.total_population, self.pseudocount * mean).to_event(
+                1
+            ),
+        )
 
 
 class MIRA_I_obs_with_scaled_Gaussian_noise(MiraPetriNetODESystem):
-    '''
+    """
     This is the same as the MIRA model, but with Scaled Gaussian noise instead of ScaledBeta noise.
-    '''
-    def __init__(self, G: mira.modeling.Model, total_population: int = 1, data_reliability: float = 4.0):
+    """
+
+    def __init__(
+        self,
+        G: mira.modeling.Model,
+        total_population: int = 1,
+        data_reliability: float = 4.0,
+    ):
+        """Initialize the model with the total population and the data reliability. The more reliable the data, the smaller the noise variance."""
         self.total_population = total_population
         self.data_reliability = data_reliability
         for param_info in G.parameters.values():
@@ -110,19 +146,25 @@ class MIRA_I_obs_with_scaled_Gaussian_noise(MiraPetriNetODESystem):
             if param_value is None:
                 param_info.value = pyro.distributions.Uniform(0.0, 1.0)
             elif isinstance(param_value, (int, float)):
-                param_info.value = pyro.distributions.Uniform(max(0.9 * param_value, 0.0), 1.1 * param_value)
+                param_info.value = pyro.distributions.Uniform(
+                    max(0.9 * param_value, 0.0), 1.1 * param_value
+                )
         super().__init__(G)
-
 
     @pyro.nn.pyro_method
     def observation_model(self, solution: Solution, var_name: str) -> None:
-        """In the observation model, I_obs is the sum of I and Iv."""
+        """In the observation model, I_obs is the sum of I and Iv.  We scale the noise by the square root of the mean, so that the noise variance is proportional to the mean."""
         if var_name == "I_obs":
             mean = solution["I"] + solution["I_v"]
         else:
             mean = solution[var_name]
-        pyro.sample(var_name, dist.Normal(mean, torch.sqrt(mean)/self.data_reliability).to_event(1))
+        pyro.sample(
+            var_name,
+            dist.Normal(mean, torch.sqrt(mean) / self.data_reliability).to_event(1),
+        )
 
     def __repr__(self):
-        par_string = ",\n\t".join([f"{get_name(p)} = {p.value}" for p in self.G.parameters.values()])
+        par_string = ",\n\t".join(
+            [f"{get_name(p)} = {p.value}" for p in self.G.parameters.values()]
+        )
         return f"{self.__class__.__name__}(\n\t{par_string},\n\ttotal_population = {self.total_population},\n\tdata_reliability = {self.data_reliability}\n)"
