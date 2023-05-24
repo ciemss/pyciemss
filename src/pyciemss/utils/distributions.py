0dbc224c31d0b3d1eee5abf9148207c309ca81b0
Sam Witty <samawitty@gmail.com>
2023-05-24 14:07:37 -0400

    not sure where Beta and TransformedDistribution ran off to.

--- src/pyciemss/utils/distributions.py
+++ src/pyciemss/utils/distributions.py
@@ -1,6 +1,7 @@
 from torch.distributions import constraints
 from torch.distributions import TransformedDistribution
 from torch.distributions.transforms import AffineTransform
+from pyro.distributions import Beta, TransformedDistribution
 
 
 class ScaledBeta(TransformedDistribution):
