5715acb90006837a58da4f4f0f4ddea0e859f7e7
Sam Witty <samawitty@gmail.com>
2023-06-23 09:54:59 -0400

    moved AMR into just one-box interface so that existing tests pass

--- src/pyciemss/PetriNetODE/interfaces.py
+++ src/pyciemss/PetriNetODE/interfaces.py
@@ -15,6 +15,7 @@ from pyciemss.utils.interface_utils import convert_to_output_format, csv_to_list
 import time
 import numpy as np
 from math import ceil
+import json
 
 import pandas as pd
 
@@ -106,6 +107,9 @@ def load_and_calibrate_and_sample_petri_model(
 
     data = csv_to_list(data_path)
 
+    model_json = json.load(f)
+    template_model = mira.metamodel.TemplateModel.from_json(model_json)
+
     model = load_petri_model(
         petri_model_or_path=petri_model_or_path,
         add_uncertainty=add_uncertainty,
@@ -152,11 +156,11 @@ def load_petri_model(
     """
 
     if add_uncertainty:
-        model = ScaledBetaNoisePetriNetODESystem.from_askenet(petri_model_or_path)
+        model = ScaledBetaNoisePetriNetODESystem.from_mira(petri_model_or_path)
         model.pseudocount = torch.tensor(pseudocount)
         return model
     else:
-        return MiraPetriNetODESystem.from_askenet(petri_model_or_path)
+        return MiraPetriNetODESystem.from_mira(petri_model_or_path)
 
 
 @setup_model.register
