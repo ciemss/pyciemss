fe6f6258c7ee99552cbee130a2ff7ae68e3a5865
Anirban Chaudhuri <75496534+anirban-chaudhuri@users.noreply.github.com>
2023-07-10 09:43:49 -0400

    Updating from main to fix model loading errors (#211)
    * Change setup to fix MIRA name error (#205)
    
    * changed mira version before bug
    
    * fix error
    
    * Add utilities for loading distributions from AMR (#200)
    
    * added mira distribution loading
    
    * added normal distribution
    
    * fixed Normal2 and Normal3
    
    * nit
    
    * added minimal mira_distribution_to_pyro test
    
    * Symbolic Rate law to Pytorch Rate law (#201)
    
    * I believe I wrote the correct code, based on experiments in the notebook. Will test next.
    
    * FAILED test/test_mira/test_rate_law.py::TestRateLaw::test_rate_law_compilation - AttributeError: 'ScaledBetaNoisePetriNetODESystem' object has no attribute 'beta'
    
    * Added Symbolic_Deriv_Experiments notebook
    
    * Something weird is happening. I can confirm that 'beta' is an attribute of ScaledBetaNoisePetriNetODESystem after setting up the model, but then it can't be found at sample time
    
    * Clarified the bug in the Symbolic derivatives notebook
    
    * Expected and actual derivative match
    
    * Time varying parameter rate law correctly read
    
    * Thought we added this already
    
    * Added kwargs to from_askenet and from_mira and compile_rate_law_p to load_petri_net
    
    * Blocked on https://github.com/indralab/mira/issues/189 but tests pass by making compile_rate_law_p False by default
    
    * Removed unnecessary pygraphviz dependency
    
    * Unit test to fail when concept name does not equal rate law symbols
    
    * All tests pass with default compile_rate_law_p = False
    
    * Merged from main. removed dependency on older version of mira
    
    * point mira to the github repo main branch
    
    * point mira to the github repo main branch
    
    * load_and_calibrate_and_sample(..., compile_rate_law_p=True) works with the caveat that the ScaledBetaNoisePetriNetODESystem solution was returning very slightly negative values, so I set mean = torch.abs(solution[var_name]) to address the issue
    
    * merged changes to MiraPetriNetODESystem and ScaledBetaNoisePetriNetODESystem from main.  ScaledBetaNoisePetriNetODESystem has default compiled_rate_law_p=True
    
    * observation_function for ScaledBetaNoisePetriNetODESystem now uses torch.maximum(solution[var_name], torch.tensor(1e-9)) to deal with overshooting derivatives
    
    * aggregate parameters is now by default opt-out, and AMR models with multiple parameters per transition can be interpreted using compile_rate_law
    
    ---------
    
    Co-authored-by: Sam Witty <samawitty@gmail.com>
    Co-authored-by: Jeremy Zucker <djinnome@gmail.com>

--- test/test_petrinet_ode/test_ode_interfaces.py
+++ test/test_petrinet_ode/test_ode_interfaces.py
@@ -65,9 +65,6 @@ class Test_Samples_Format(unittest.TestCase):
             interventions=self.interventions,
         )
 
-
-    
-
     def test_samples_type(self):
         """Test that `samples` is a Pandas DataFrame"""
         for s in [self.samples, self.calibrated_samples]:
@@ -272,9 +269,9 @@ class TestODEInterfaces(unittest.TestCase):
         timepoints = [1.0, 1.1, 1.2, 1.3]
         num_samples = 3
         initial_state = {
-            "Susceptible": 0.99,
-            "Infected": 0.01,
-            "Recovered": 0.0,
+            "S": 0.99,
+            "I": 0.01,
+            "R": 0.0,
         }
         expected_intervened_samples = pd.read_csv('test/test_petrinet_ode/expected_intervened_samples.csv')
         actual_intervened_samples = load_and_sample_petri_model(ASKENET_PATH, num_samples, timepoints, interventions = interventions, start_state=initial_state)
@@ -288,9 +285,9 @@ class TestODEInterfaces(unittest.TestCase):
         timepoints = [1.0, 1.1, 1.2, 1.3]
         num_samples = 3
         initial_state = {
-            "Susceptible": 0.99,
-            "Infected": 0.01,
-            "Recovered": 0.0,
+            "S": 0.99,
+            "I": 0.01,
+            "R": 0.0,
         }
         expected_intervened_samples = pd.read_csv('test/test_petrinet_ode/expected_intervened_samples.csv')
         data_path = 'test/test_petrinet_ode/data.csv'
