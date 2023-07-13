aa74c9b0d856678c61614047225937fc45f0b7cd
Joseph Cottam <joseph.cottam@pnnl.gov>
2023-07-13 16:13:08 -0400

    First version that writes a list of expectations and can execute it.  VERY BAD

--- test/test_mira/test_amr.py
+++ test/test_mira/test_amr.py
@@ -1,70 +1,130 @@
 import json
 import unittest
-import traceback
+from pathlib import Path
+import json
+import dataclasses
+from urllib.request import urlopen
+
 from pyciemss.PetriNetODE.interfaces import (
     load_petri_model,
     load_and_sample_petri_model,
-    load_and_calibrate_and_sample_petri_model
-    )
+    load_and_calibrate_and_sample_petri_model,
+)
 from pyciemss.PetriNetODE.base import ScaledBetaNoisePetriNetODESystem
-import warnings
 import pandas as pd
+from pathlib import Path
+
+_config_file = Path(__file__).parent / "AMR_expectations.json"
+_report_file = Path(__file__).parent / "AMR_expectations_report.json"
+AMR_URL_TEMPLATE = "https://raw.githubusercontent.com/DARPA-ASKEM/experiments/main/thin-thread-examples/mira_v2/biomodels/{biomodel_id}/model_askenet.json"
+AMR_ROOT = Path(__file__).parent / ".." / "models" / "AMR_examples" / "biomodels"
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if dataclasses.is_dataclass(o):
+            return dataclasses.asdict(o)
+        return super().default(o)
+
+
+# ---------------------------
+@dataclasses.dataclass(init=False)
+class Configuration:
+    id: str
+    source_url: str
+    tests_pass: list
+    tests_fail: list
+
+    def __init__(self, id):
+        self.id = id
+        self.source_url = AMR_URL_TEMPLATE.format(biomodel_id=id)
+        self.tests_pass = list()
+        self.tests_fail = list()
+
+    @staticmethod
+    def from_json(spec):
+        v = Configuration(spec["id"])
+        v.source_url == spec["source_url"]
+        v.tests_pass = spec["tests_pass"]
+        v.tests_fail = spec["tests_fail"]
+        return v
+
+
+def try_loading_biomodel(config: Configuration, context: any):
+    source_file = (AMR_ROOT / config.id / "model_askenet.json").absolute()
+    if not Path(source_file).exists():
+        config.tests_fail.append("File not found")
+        return
+
+    try:
+        model = load_petri_model(str(source_file), compile_rate_law_p=True)
+        context.assertIsNotNone(model)
+        context.assertIsInstance(model, ScaledBetaNoisePetriNetODESystem)
+        config.tests_pass.append("load_petri_model")
+    except KeyError:
+        config.tests_fail.append("load_petri_model")
+        return
+
+    try:
+        model = load_and_sample_petri_model(
+            str(source_file),
+            compile_rate_law_p=True,
+            num_samples=2,
+            timepoints=[0.0, 0.1, 0.2, 0.3],
+        )
+        context.assertIsNotNone(model)
+        context.assertIsInstance(model, pd.DataFrame)
+        config.tests_pass.append("load_and_sample_petri_model")
+    except Exception:
+        config.tests_fail.append("load_and_sample_petri_model")
+        return
+
 
 class TestAMR(unittest.TestCase):
     def setUp(self):
-        AMR_URL_TEMPLATE= "https://raw.githubusercontent.com/DARPA-ASKEM/experiments/main/thin-thread-examples/mira_v2/biomodels/{biomodel_id}/model_askenet.json"
-        AMR_PATH_TEMPLATE = "test/models/AMR_examples/biomodels/{biomodel_id}/model_askenet.json"
-        biomodels = """BIOMD0000000249	BIOMD0000000716	BIOMD0000000949	BIOMD0000000956	BIOMD0000000960	BIOMD0000000964	BIOMD0000000971	BIOMD0000000976	BIOMD0000000979	BIOMD0000000982	BIOMD0000000988	MODEL1008060000	MODEL1805230001	MODEL2111170001 BIOMD0000000294	BIOMD0000000717	BIOMD0000000950	BIOMD0000000957	BIOMD0000000962	BIOMD0000000969	BIOMD0000000972	BIOMD0000000977	BIOMD0000000980	BIOMD0000000983	BIOMD0000000991	MODEL1008060002	MODEL1808280006
-BIOMD0000000715	BIOMD0000000726	BIOMD0000000955	BIOMD0000000958	BIOMD0000000963	BIOMD0000000970	BIOMD0000000974	BIOMD0000000978	BIOMD0000000981	BIOMD0000000984	BIOMD0000001045	MODEL1805220001	MODEL1808280011""".split()
-        self.biomodels_tests = {biomodel_id: dict(source_url=AMR_URL_TEMPLATE.format(biomodel_id=biomodel_id),
-                                                  source_file=AMR_PATH_TEMPLATE.format(biomodel_id=biomodel_id),
-                                                  tests_pass=list(),
-                                                  tests_fail=list())
-                                for biomodel_id in biomodels}
+        with open(_config_file) as f:
+            self.configs = json.load(f)
 
-    def test_load_biomodels(self):
-        """Test how if all biomodels can be loaded in the AMR format."""
-        for biomodel_id, biomodel in self.biomodels_tests.items():
-            try:
-                model = load_petri_model(biomodel["source_file"], compile_rate_law_p=True)
-                self.assertIsNotNone(model)
-                self.assertIsInstance(model, ScaledBetaNoisePetriNetODESystem)
-                self.biomodels_tests[biomodel_id]["tests_pass"].append('load_petri_model')
-                    
-            except AttributeError as e:
-                warnings.warn(f"{biomodel_id} {str(e)}")
-                tb = traceback.format_exc()
-                warnings.warn(tb)
-            except KeyError as k:
-                warnings.warn(f"{biomodel_id} {str(k)}")
-                self.biomodels_tests[biomodel_id]["tests_fail"].append('load_petri_model')
+    def test_remotes_changed(self):
+        changes = []
+        for src in self.configs:
+            config = Configuration.from_json(src)
+            source_file = AMR_ROOT / config.id / "model_askenet.json"
 
-            try:
-                model = load_and_sample_petri_model(
-                    biomodel["source"], compile_rate_law_p=True, num_samples=2, timepoints=[0.0, 0.1, 0.2, 0.3])
-                self.assertIsNotNone(model)
-                self.assertIsInstance(model, pd.DataFrame)
-                self.biomodels_tests[biomodel_id]["tests_pass"].append('load_and_sample_petri_model')
-            except Exception as e:
-                self.biomodels_tests[biomodel_id]["tests_fail"].append('load_and_sample_petri_model')
-                warnings.warn(f"{biomodel_id} {str(e)}")
-                tb = traceback.format_exc()
-                warnings.warn(tb)
-                
-            try:
-                with open(biomodel["source_file"], "r") as f:
-                    file_content = json.load(f)
-                    file_str = json.dumps(file_content, indent=2)
-                with open(biomodel["source_url"], "r") as f:
-                    url_content = json.load(f)
-                    url_str = json.dumps(url_content, indent=2)
-                self.assertEqual(file_str, url_str)
-                self.biomodels_tests[biomodel_id]["tests_pass"].append('url_against_file')
-            except Exception as e:
-                self.biomodels_tests[biomodel_id]["tests_fail"].append('url_against_file')
-                warnings.warn(f"{biomodel_id} {str(e)}")
-                tb = traceback.format_exc()
-                warnings.warn(tb)
-        json.dump(self.biomodels_tests, open("test/test_mira/test_amr.json", "w"), indent=2)
+            with open(source_file, "r") as f:
+                file_content = json.load(f)
+                file_str = json.dumps(file_content, indent=2)
 
-        
+            f = urlopen(config.source_url)
+            url_content = json.load(f)
+            url_str = json.dumps(url_content, indent=2)
+
+            if url_str != file_str:
+                changes.append(config.id)
+
+        if len(changes) > 0:
+            print(f"Files changed: {', '.join(changes)}")
+
+        self.assertEqual([], changes, "URLs and local source out of sync")
+
+    def test_configs(self):
+        print("Starting test...")
+        print(f"ABS PATH: {AMR_ROOT.absolute()}")
+        results = []
+        failures = []
+        for config in self.configs:
+            ref = Configuration.from_json(config)
+            result = Configuration.from_json(config)
+
+            try_loading_biomodel(result, self)
+
+            # self.assertEqual(ref, result)
+
+            results.append(result)
+            if set(result.tests_fail) != set(ref.tests_fail):
+                failures.append(result)
+
+        with open(_report_file, "w") as f:
+            json.dump(results, f, cls=JSONEncoder, indent=3)
+
+        self.assertIs(len(failures), 0, "Unexpected failures detected")
--- test/utils/generate_amr_expectations.py
+++ test/utils/generate_amr_expectations.py
@@ -1,4 +1,5 @@
 import json
+import sys
 import argparse
 import dataclasses
 import warnings
@@ -14,60 +15,35 @@ from pyciemss.PetriNetODE.base import ScaledBetaNoisePetriNetODESystem
 import pandas as pd
 from pathlib import Path
 
-
 AMR_URL_TEMPLATE = "https://raw.githubusercontent.com/DARPA-ASKEM/experiments/main/thin-thread-examples/mira_v2/biomodels/{biomodel_id}/model_askenet.json"
-AMR_ROOT = Path("..") / "models" / "AMR_examples" / "biomodels"
-
-biomodels = [
-    "BIOMD0000000249",
-    "BIOMD0000000716",
-    "BIOMD0000000949",
-    "BIOMD0000000956",
-    "BIOMD0000000960",
-    "BIOMD0000000964",
-    "BIOMD0000000971",
-    "BIOMD0000000976",
-    "BIOMD0000000979",
-    "BIOMD0000000982",
-    "BIOMD0000000988",
-    "MODEL1008060000",
-    "MODEL1805230001",
-    "MODEL2111170001",
-    "BIOMD0000000294",
-    "BIOMD0000000717",
-    "BIOMD0000000950",
-    "BIOMD0000000957",
-    "BIOMD0000000962",
-    "BIOMD0000000969",
-    "BIOMD0000000972",
-    "BIOMD0000000977",
-    "BIOMD0000000980",
-    "BIOMD0000000983",
-    "BIOMD0000000991",
-    "MODEL1008060002",
-    "MODEL1808280006",
-    "BIOMD0000000715",
-    "BIOMD0000000726",
-    "BIOMD0000000955",
-    "BIOMD0000000958",
-    "BIOMD0000000963",
-    "BIOMD0000000970",
-    "BIOMD0000000974",
-    "BIOMD0000000978",
-    "BIOMD0000000981",
-    "BIOMD0000000984",
-    "BIOMD0000001045",
-    "MODEL1805220001",
-    "MODEL1808280011",
-]
+AMR_ROOT = Path(__file__).parent / ".." / "models" / "AMR_examples" / "biomodels"
 
 
-def test_load_biomodels(config):
+# ---------------------------
+@dataclasses.dataclass(init=False)
+class Configuration:
+    id: str
+    source_url: str
+    tests_pass: list
+    tests_fail: list
+
+    def __init__(self, id):
+        self.id = id
+        self.source_url = AMR_URL_TEMPLATE.format(biomodel_id=id)
+        self.tests_pass = list()
+        self.tests_fail = list()
+
+
+def try_loading_biomodel(config):
     try:
-        model = load_petri_model(config.source_file, compile_rate_law_p=True)
-        config.assertIsNotNone(model)
-        config.assertIsInstance(model, ScaledBetaNoisePetriNetODESystem)
-        config.tests_pass.append("load_petri_model")
+        source_file = str(AMR_ROOT / config.id / "model_askenet.json")
+        model = load_petri_model(source_file, compile_rate_law_p=True)
+        if model is None:
+            config.tests_fail.append("load_petri_model")
+        elif not isinstance(model, ScaledBetaNoisePetriNetODESystem):
+            config.tests_fail.append("load_petri_model")
+        else:
+            config.tests_pass.append("load_petri_model")
     except AttributeError as e:
         warnings.warn(f"{config.id} {str(e)}")
         tb = traceback.format_exc()
@@ -77,15 +53,18 @@ def test_load_biomodels(config):
         config.tests_fail.append("load_petri_model")
 
     try:
-        model = load_and_sample_petri_model(
-            config.source,
+        result = load_and_sample_petri_model(
+            source_file,
             compile_rate_law_p=True,
             num_samples=2,
             timepoints=[0.0, 0.1, 0.2, 0.3],
         )
-        config.assertIsNotNone(model)
-        config.assertIsInstance(model, pd.DataFrame)
-        config.tests_pass.append("load_and_sample_petri_model")
+        if result is None:
+            config.tests_fail.append("load_and_sample_petri_model")
+        elif not isinstance(result, pd.DataFrame):
+            config.tests_fail.append("load_and_sample_petri_model")
+        else:
+            config.tests_pass.append("load_and_sample_petri_model")
     except Exception as e:
         config.tests_fail.append("load_and_sample_petri_model")
         warnings.warn(f"{config.id} {str(e)}")
@@ -93,14 +72,17 @@ def test_load_biomodels(config):
         warnings.warn(tb)
 
     try:
-        with open(config.source_file, "r") as f:
+        with open(source_file, "r") as f:
             file_content = json.load(f)
             file_str = json.dumps(file_content, indent=2)
 
         f = urlopen(config.source_url)
         url_content = json.load(f)
         url_str = json.dumps(url_content, indent=2)
-        config.assertEqual(file_str, url_str)
+
+        if file_str != url_str:
+            raise ValueError(f"File and URL content do not match for {config.id}")
+
         config.tests_pass.append("url_against_file")
     except Exception as e:
         config.tests_fail.append("url_against_file")
@@ -109,67 +91,66 @@ def test_load_biomodels(config):
         warnings.warn(tb)
 
 
-# ---------------------------
-@dataclasses.dataclass(init=False)
-class TestConfig:
-    id: str
-    source_url: str
-    source_file: Path
-    tests_pass: list
-    tests_fail: list
+if __name__ == "__main__":
 
-    def __init__(self, id):
-        self.id = id
-        self.source_url = AMR_URL_TEMPLATE.format(biomodel_id=id)
-        self.source_file = str(AMR_ROOT / id / "model_askenet.json")
-        self.tests_pass = list()
-        self.tests_fail = list()
+    class JSONEncoder(json.JSONEncoder):
+        def default(self, o):
+            if dataclasses.is_dataclass(o):
+                return dataclasses.asdict(o)
+            return super().default(o)
 
-
-class JSONEncoder(json.JSONEncoder):
-    def default(self, o):
-        if dataclasses.is_dataclass(o):
-            return dataclasses.asdict(o)
-        return super().default(o)
-
-
-parser = argparse.ArgumentParser(description="Generate expected results JSON file.")
-parser.add_argument(
-    "target",
-    help="Where to write the expectations to. WILL NOT OVERWRITE.",
-    default="AMR_expectations.json",
-    type=Path,
-)
-parser.add_argument(
-    "-r",
-    "--reference",
-    help="Prepare a diff against this file.",
-    default="AMR_expectations_old.json",
-    type=Path,
-)
-parser.add_argument(
-    "--test",
-    help="Only process <test> models, skips early file test",
-    default=None,
-    type=int,
-)
-args = parser.parse_args()
-
-if not args.test and args.target.exists():
-    raise ValueError(
-        "Target file exists before run. Move/rename/delete before executing."
+    parser = argparse.ArgumentParser(description="Generate expected results JSON file.")
+    parser.add_argument(
+        "-t",
+        "--target",
+        help="Where to write the expectations to. WILL NOT OVERWRITE.",
+        default=Path(".") / ".." / "test_mira" / "AMR_expectations.json",
+        required=False,
+        type=Path,
     )
+    parser.add_argument(
+        "-r",
+        "--reference",
+        help="Prepare a diff against this file.",
+        default=None,
+        required=False,
+        type=Path,
+    )
+    parser.add_argument(
+        "--test",
+        help="Only process <test> models, skips early file test",
+        default=None,
+        required=False,
+        type=int,
+    )
+    parser.add_argument("--quiet", help="Suppress warning output", action="store_true")
 
-tests = [TestConfig(model_id) for model_id in biomodels][: args.test]
+    args = parser.parse_args()
 
-for config in tests:
-    test_load_biomodels(config)
+    if not args.test and args.target.exists():
+        raise ValueError(
+            "Target file exists before run. Move/rename/delete before executing."
+        )
 
-try:
-    with open(args.target, "x") as f:
-        json.dump(tests, f, cls=JSONEncoder, indent=3)
-except Exception:
-    print(json.dumps(tests, cls=JSONEncoder, indent=3))
-    raise Exception
+    biomodels = [f.name for f in AMR_ROOT.glob("*")]
 
-# TODO: Prepare diff report
+    tests = [Configuration(model_id) for model_id in biomodels][: args.test]
+
+    for config in tests:
+        with warnings.catch_warnings():
+            if args.quiet:
+                warnings.simplefilter("ignore")
+            try_loading_biomodel(config)
+
+    try:
+        with open(args.target, "x") as f:
+            json.dump(tests, f, cls=JSONEncoder, indent=3)
+    except FileExistsError:
+        print("\n\nTarget file already exists. NOT OVERWRITTING.  Output below:")
+        print(f"\tTarget file: {args.target}\n")
+        print(json.dumps(tests, cls=JSONEncoder, indent=3))
+        sys.exit(-1)
+
+    if args.reference is not None:
+        pass
+        # TODO: Prepare diff report
