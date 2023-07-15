01be6a95e41bc7e0d674bb591e8f9a8df6df9e6b
Sam Witty <samawitty@gmail.com>
2023-07-15 11:33:51 -0400

    name change in test

--- test/test_petrinet_ode/test_ode_interfaces.py
+++ test/test_petrinet_ode/test_ode_interfaces.py
@@ -65,7 +65,7 @@ class TestSamplesFormat(unittest.TestCase):
             cls.num_samples,
             timepoints=timepoints,
             num_iterations=2,
-            deterministically_inferred_parameters=["beta"],
+            deterministic_learnable_parameters=["beta"],
         )["data"]
 
         cls.interventions = [(1., "beta", 1.0), (2.1, "gamma", 0.1)]
