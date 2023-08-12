352f5eb4cea70f4434b0688296b391579b4a9c7b
Joseph Cottam <joseph.cottam@pnnl.gov>
2023-08-12 10:14:34 -0700

    Select exemplar trajectories by characteristics.

--- src/pyciemss/visuals/trajectories.py
+++ src/pyciemss/visuals/trajectories.py
@@ -1,4 +1,4 @@
-from typing import Dict, Union, Any, Optional
+from typing import Dict, Union, Any, Optional, Literal
 
 from numbers import Integral, Number
 import pandas as pd
@@ -6,84 +6,116 @@ import numpy as np
 import re
 
 from . import vega
+from statsmodels.tsa.stattools import grangercausalitytests
 
 
-def nice_df(df):
-    """Standardizes dataframe setup, imputing columns if possible and setting index."""
+def select_traces(
+    traces,
+    *,
+    select_by: Literal["mean", "var", "granger"] = "mean",
+    keep: Union[str, list, all] = all,
+    drop: Union[str, list, None] = None,
+    relabel: Optional[Dict[str, str]] = None,
+):
+    """Picks an actual trajectory based on the envelope of trajectories and a selection criteria
 
-    if df.index.name is not None or df.index.names != [None]:
-        df = df.reset_index()
-
-    if "sample_id" not in df.columns:
-        df = df.assign(sample_id=0)
-
-    if "timepoint_id" not in df.columns:
-        df = df.assign(timepoint_id=range(len(df)))
-
-    timepoint_cols = [
-        c for c in df.columns if c.startswith("timepoint_") and c != "timepoint_id"
-    ]
-    if len(timepoint_cols) == 0:
-        df = df.assign(timepoint=df.timepoint_id)
-    elif len(timepoint_cols) > 1:
-        raise ValueError(
-            f"Cannot work with multiple timepoint formats. Found {timepoint_cols}"
-        )
-    else:
-        df = df.assign(timepoint=df[timepoint_cols[0]]).drop(columns=timepoint_cols)
-
-    df = df.drop(columns=["timepoint_id"]).set_index(["timepoint", "sample_id"])
-    return df
-
-
-def clean_nans(
-    entries: list[dict], *, parent: bool = True, replace: Optional[Any] = None
-) -> list[dict]:
-    """Clean list of entries for json serailizazation. ONLY LOOKS ONE LEVEL DOWN.
     Args:
-        entries (list[dict]): Entries to clean (list of json dictionaries)
-        parent (bool, optional): Replace parent (default) or key value?
-        replace (Any, optional): Replace with this value.  If None, deletes.
-    Returns:
-        _type_: _description_
+        traces: Dataframe of traces
+        subset: only keep subset columns
+        select_by -- Method for selecting a trajectory
+          - "mean" -- Trajectory closest to the mean-line of the envelope of all trajectories
+          - "var" -- Trajectory that has the most-similar dynamics to the mean-line of the envelope of all trajectories
+          - "granger" -- Trajectory that "best predicts" the mean-line of the envelope (by the grangercausalitytest, maxlag=10)
+
+        keep (str, list, all): Only keep some of the 'distributions' based on keys/values.
+           - Default is the 'all' function, and it keeps all columns
+           - If a string is present, it is treated as a regex and matched against the columns. Matches are kept.
+           - Otherwise, assumed to be a list-like of columns to keep
+           If keep is specified, the color scale ordering follows the kept order.
+        drop (str, list, None): Drop specific columns from 'distributions' (applied AFTER keep)
+          - Default is 'None', keeping all columns
+          - If a string is present, it is treated as a regex and matched against the columns. Matches are dropped.
+          - Otherwise, assumed to be a list-like of columns to drop
+          Drop will not error if a name specified is not present at drop time.
+        relabel (None, Dict[str, str]): Relabel elements for rendering.  Happens
+            after keep & drop.
+
     """
+    traces_df = _nice_df(traces)
+    traces_df = _keep_drop_rename(traces_df, keep, drop, relabel)
 
-    def is_nan(v):
-        try:
-            return np.isnan(v)
-        except Exception:
-            return False
+    # get average line
+    examplary_line_df = (
+        traces_df.melt(ignore_index=False, var_name="trajectory")
+        .set_index("trajectory", append=True)
+        .groupby(level=["trajectory", "timepoint"])
+        .mean()
+        .reset_index()
+    )
 
-    def has_nan(d):
-        for v in d.values():
-            if is_nan(v):
-                return True
-        return False
+    def granger_fun(x):
+        # first column is the column to compare to
+        # not sure what maxlag value to use
+        # return ssr-based-F test p value
+        granger_value = grangercausalitytests(x[["mean_value", "value"]], maxlag=[10])[
+            10
+        ][0]["ssr_ftest"][1]
+        return granger_value
 
-    def maybe_replace(d, replace_with):
-        fix = [k for k, v in d.items() if is_nan(v)]
-        for k in fix:
-            d[k] = replace_with
-        return d
+    # all data, melted
+    melt_all = traces_df.melt(ignore_index=False, var_name="trajectory").reset_index()
+    # add value of average line to original df as column
+    merged_all_mean = pd.merge(
+        melt_all, examplary_line_df, on=["trajectory", "timepoint"]
+    ).rename(columns={"value_x": "value", "value_y": "mean_value"})
+    # get distance from average line for each sample/timepoint/trajectory
+    merged_all_mean["distance_mean"] = abs(
+        merged_all_mean["mean_value"] - merged_all_mean["value"]
+    )
+    # get sum of distance from mean for all timepoints in sample/trajectory
+    group_examplary = merged_all_mean.set_index(
+        ["trajectory", "timepoint", "sample_id"], append=True
+    ).groupby(level=["trajectory", "sample_id"])
 
-    def remove_nan(d):
-        fix = [k for k, v in d.items() if is_nan(v)]
-        for k in fix:
-            del d[k]
-        return d
+    # get sum and variable of each trajectory (rabbit or wolf) and sample id group
+    if select_by == "mean":
+        sum_examplary = (
+            group_examplary.mean().reset_index()
+        )  # get only min distance from each trajectory type (rabbit or worlf) back
+        best_sample_id = sum_examplary.loc[
+            sum_examplary.groupby("trajectory").distance_mean.idxmin()
+        ][["sample_id", "trajectory"]]
 
-    if replace is None:
-        if parent:
-            entries = [e for e in entries if not has_nan(e)]
-        else:
-            entries = [remove_nan(e, replace) for e in entries]
-    else:
-        if parent:
-            entries = [replace if has_nan(e) else e for e in entries]
-        else:
-            entries = [maybe_replace(e, replace) for e in entries]
+    elif select_by == "var":
+        sum_examplary = (
+            group_examplary.var().reset_index()
+        )  # get only min distance from each trajectory type (rabbit or worlf) back
+        best_sample_id = sum_examplary.loc[
+            sum_examplary.groupby("trajectory").distance_mean.idxmin()
+        ][["sample_id", "trajectory"]]
 
-    return entries
+    elif select_by == "granger":
+        granger_examplary = group_examplary.apply(lambda x: granger_fun(x))
+        sum_examplary = pd.DataFrame({"granger": granger_examplary})
+        sum_examplary = sum_examplary.reset_index()
+        best_sample_id = sum_examplary.loc[
+            sum_examplary.groupby("trajectory").granger.idxmin()
+        ][["sample_id", "trajectory"]]
+
+    # only keep sample id's from 'best' lines
+    only_examplary_line = pd.merge(
+        melt_all, best_sample_id, on=["sample_id", "trajectory"], how="right"
+    )
+
+    # get into dataframe in correct foramt for trajectories traces argument
+    only_examplary_line = only_examplary_line.drop(
+        columns=["sample_id"], errors="ignore"
+    )
+    examplary_line = only_examplary_line.pivot_table(
+        values="value", index="timepoint", columns="trajectory"
+    )
+    examplary_line["timepoint_id"] = examplary_line.index
+    return examplary_line
 
 
 def trajectories(
@@ -108,24 +140,24 @@ def trajectories(
     TODO: Handle the 'No distributions' case
 
     Args:
-        observations (None, pd.DataFrame): Dataframe formatted per
+        distributions (pd.DataFrame): Dataframe formatted per
         pyciemss.utils.interface_utils.convert_to_output_format
            These will be plotted as spans based on the qlow/qhigh parameters
         traces (None, pd.DataFrame): Example trajectories to plot.
         points (None, pd.DataFrame): Example points to plot (joined by lines)
         markers (None, list[Number]): Timepoint markers. Key is the label, value is the timepoint
-        keep (str, list, all): Only keep some of the 'observations' based on keys/values.
+        keep (str, list, all): Only keep some of the 'distributions' based on keys/values.
            - Default is the 'all' function, and it keeps all columns
            - If a string is present, it is treated as a regex and matched against the columns. Matches are kept.
            - Otherwise, assumed to be a list-like of columns to keep
            If keep is specified, the color scale ordering follows the kept order.
-        drop (str, list, None): Drop specific columns (applied AFTER keep)
-          - Defaul is 'None', keeping all columns
+        drop (str, list, None): Drop specific columns from 'distributions' (applied AFTER keep)
+          - Default is 'None', keeping all columns
           - If a string is present, it is treated as a regex and matched against the columns. Matches are dropped.
           - Otherwise, assumed to be a list-like of columns to drop
           Drop will not error if a name specified is not present at drop time.
         relabel (None, Dict[str, str]): Relabel elements for rendering.  Happens
-            after key keep & drop.
+            after keep & drop.
         colors: Use the specified colors as a post-relable keyed dictionary to vega-valid color.
            Mapping to None or not includding a mapping will drop that sequence
         qlow (float): Lower percentile to use in obsersvation distributions
@@ -136,31 +168,11 @@ def trajectories(
     if relabel is None:
         relabel = dict()
 
-    distributions = nice_df(distributions)
-    if traces is not None:
-        traces = nice_df(traces)
-    if points is not None:
-        points = nice_df(points)
+    distributions = _nice_df(distributions)
+    traces = _nice_df(traces)
+    points = _nice_df(points)
 
-    if keep == all:
-        keep = distributions.columns
-    elif isinstance(keep, str):
-        keep = [k for k in distributions.columns if re.match(keep, k)]
-    else:
-        keep = keep
-
-    if drop is None:
-        drop = []
-    elif isinstance(drop, str):
-        drop = [k for k in distributions.columns if re.match(drop, k)]
-    else:
-        drop = drop
-
-    distributions = (
-        distributions.filter(items=keep)
-        .drop(columns=drop, errors="ignore")
-        .rename(columns=relabel)
-    )
+    distributions = _keep_drop_rename(distributions, keep, drop, relabel)
 
     if colors:
         keep = [k for k in distributions.columns if colors.get(k, None) is not None]
@@ -219,17 +231,17 @@ def trajectories(
 
     schema = vega.load_schema("trajectories.vg.json")
     schema["data"] = vega.replace_named_with(
-        schema["data"], "distributions", ["values"], clean_nans(distributions)
+        schema["data"], "distributions", ["values"], _clean_nans(distributions)
     )
 
     schema["data"] = vega.replace_named_with(
-        schema["data"], "points", ["values"], clean_nans(points)
+        schema["data"], "points", ["values"], _clean_nans(points)
     )
     schema["data"] = vega.replace_named_with(
-        schema["data"], "traces", ["values"], clean_nans(traces)
+        schema["data"], "traces", ["values"], _clean_nans(traces)
     )
     schema["data"] = vega.replace_named_with(
-        schema["data"], "markers", ["values"], clean_nans(markers)
+        schema["data"], "markers", ["values"], _clean_nans(markers)
     )
 
     if colors is not None:
@@ -265,3 +277,127 @@ def trajectories(
 # trajectories(prior_samples, tspan, obs_keys = ".*_sol") == ['Rabbits_sol', 'Wolves_sol']
 # trajectories with and without join_points
 # combinations of calls (colors, colors+relable, colors+subset, relable+subset, relabel+colors+subset, etc)
+
+
+# --------------- Shared utilities
+
+
+def _nice_df(df):
+    """Internal utility.  Standardizes dataframe setup, imputing columns if possible and setting index."""
+    if df is None:
+        return df
+
+    if df.index.name is not None or df.index.names != [None]:
+        df = df.reset_index()
+
+    if "sample_id" not in df.columns:
+        df = df.assign(sample_id=0)
+
+    if "timepoint_id" not in df.columns:
+        df = df.assign(timepoint_id=range(len(df)))
+
+    timepoint_cols = [
+        c for c in df.columns if c.startswith("timepoint_") and c != "timepoint_id"
+    ]
+    if len(timepoint_cols) == 0:
+        df = df.assign(timepoint=df.timepoint_id)
+    elif len(timepoint_cols) > 1:
+        raise ValueError(
+            f"Cannot work with multiple timepoint formats. Found {timepoint_cols}"
+        )
+    else:
+        df = df.assign(timepoint=df[timepoint_cols[0]]).drop(columns=timepoint_cols)
+
+    df = df.drop(columns=["timepoint_id"]).set_index(["timepoint", "sample_id"])
+    return df
+
+
+def _clean_nans(
+    entries: list[dict], *, parent: bool = True, replace: Optional[Any] = None
+) -> list[dict]:
+    """Internal Utlitiy. Clean list of entries for json serailizazation.
+    ONLY LOOKS ONE LEVEL DOWN.
+
+    Args:
+        entries (list[dict]): Entries to clean (list of json dictionaries)
+        parent (bool, optional): Replace parent (default) or key value?
+        replace (Any, optional): Replace with this value.  If None, deletes.
+    Returns:
+        _type_: _description_
+    """
+
+    def is_nan(v):
+        try:
+            return np.isnan(v)
+        except Exception:
+            return False
+
+    def has_nan(d):
+        for v in d.values():
+            if is_nan(v):
+                return True
+        return False
+
+    def maybe_replace(d, replace_with):
+        fix = [k for k, v in d.items() if is_nan(v)]
+        for k in fix:
+            d[k] = replace_with
+        return d
+
+    def remove_nan(d):
+        fix = [k for k, v in d.items() if is_nan(v)]
+        for k in fix:
+            del d[k]
+        return d
+
+    if replace is None:
+        if parent:
+            entries = [e for e in entries if not has_nan(e)]
+        else:
+            entries = [remove_nan(e, replace) for e in entries]
+    else:
+        if parent:
+            entries = [replace if has_nan(e) else e for e in entries]
+        else:
+            entries = [maybe_replace(e, replace) for e in entries]
+
+    return entries
+
+
+def _keep_drop_rename(df, keep, drop, relabel):
+    """
+    Internal utility: Implements consistent keep/drop/rename logic
+    for use across several utilities
+    """
+
+    if keep == all:
+        keep = df.columns
+    elif isinstance(keep, str):
+        try:
+            m = re.compile(keep)
+        except Exception as e:
+            raise ValueError(f"Error compiling 'keep' expression: {str(e)}")
+
+        keep = [c for c in df.columns if m.match(c)]
+    else:
+        keep = keep
+
+    if drop is None:
+        drop = []
+    elif isinstance(drop, str):
+        try:
+            m = re.compile(drop)
+        except Exception as e:
+            raise ValueError(f"Error compiling 'drop' expression: {str(e)}")
+
+        drop = [c for c in df.columns if m.match(c)]
+    else:
+        drop = drop
+
+    relabel = {} if relabel is None else relabel
+
+    return (
+        df.filter(items=keep)
+        .drop(columns=drop, errors="ignore")
+        .rename(columns=relabel)
+    )
