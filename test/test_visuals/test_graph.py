1505dd1c0a3401d05ef40c0e9279133dc7c7a227
Oostrom, Marjolein T <marjolein.oostrom@pnnl.gov>
2023-10-16 17:14:11 -0700

    add to coverage with spring graph

--- test/test_visuals/test_graph.py
+++ test/test_visuals/test_graph.py
@@ -13,27 +13,31 @@ import random
 
 _data_root = Path(__file__).parent.parent / "data"
 
+def rand_attributions(self):
+    possible = "ABCD"
+    return random.sample(possible, random.randint(1, len(possible)))
+
+def rand_label():
+    return random.randint(1, 10)
+
+def rand_group():
+    possible = "TUVWXYZ"
+    return random.sample(possible, 1)[0]
 
 class TestGraphs(unittest.TestCase):
-    def rand_attributions(self):
-        possible = "ABCD"
-        return random.sample(possible, random.randint(1, len(possible)))
 
-    def rand_label(self):
-        return random.randint(1, 10)
-    
     def test_graph_attribute(self):
         g = nx.generators.barabasi_albert_graph(5, 3)
-        node_properties = {n: {"attribution": self.rand_attributions(), 
-                            "label": self.rand_label()}
+        node_properties = {n: {"attribution": rand_attributions(), 
+                            "label": rand_label()}
                             for n in g.nodes()}
 
-        edge_attributions = {e: {"attribution": self.rand_attributions()} for e in g.edges()}
+        edge_attributions = {e: {"attribution": rand_attributions()} for e in g.edges()}
 
         nx.set_node_attributes(g, node_properties)
         nx.set_edge_attributes(g, edge_attributions)
         gjson = nx.json_graph.node_link_data(g)
-        schema = graphs.attributed_graph(g)
+
         new_schema = plots.attributed_graph(g)
         self.assertEqual(
             len(vega.find_named(new_schema['data'], "node-data")['values']),  len(gjson["nodes"])
@@ -45,8 +49,8 @@ class TestGraphs(unittest.TestCase):
     def test_edge_missing_attribute(self):
         with self.assertRaises(ValueError):
                 g = nx.generators.barabasi_albert_graph(5, 3)
-                node_properties = {n: {"attribution": self.rand_attributions(), 
-                                    "label": self.rand_label()}
+                node_properties = {n: {"attribution": rand_attributions(), 
+                                    "label": rand_label()}
                                     for n in g.nodes()}
                 nx.set_node_attributes(g, node_properties)
                 schema = graphs.attributed_graph(g)
@@ -55,10 +59,10 @@ class TestGraphs(unittest.TestCase):
     def test_node_missing_attribute(self):
         with self.assertRaises(ValueError):
                 g = nx.generators.barabasi_albert_graph(5, 3)
-                node_properties = {n: {"label": self.rand_label()}
+                node_properties = {n: {"label": rand_label()}
                                     for n in g.nodes()}
 
-                edge_attributions = {e: {"attribution": self.rand_attributions()} for e in g.edges()}
+                edge_attributions = {e: {"attribution": rand_attributions()} for e in g.edges()}
 
                 nx.set_node_attributes(g, node_properties)
                 nx.set_edge_attributes(g, edge_attributions)
@@ -67,18 +71,8 @@ class TestGraphs(unittest.TestCase):
 
 
 class TestSpringGraph(unittest.TestCase):
-    def rand_attributions(self):
-        possible = "ABCD"
-        return random.sample(possible, random.randint(1, len(possible)))
-
-    def rand_label(self):
-        return random.randint(1, 10)
-    
-    def test_graph_attribute(self):
+    def fake_network(self):
         g = nx.generators.barabasi_albert_graph(5, 3)
-        def rand_group():
-            possible = "TUVWXYZ"
-            return random.sample(possible, 1)[0]
 
         node_properties = {n: {"group": rand_group(), 
                             "fx": random.randint(1, 100),
@@ -92,9 +86,13 @@ class TestSpringGraph(unittest.TestCase):
         nx.set_edge_attributes(g, edge_attributions)
         nx.set_node_attributes(g, {k:f"n{i}" for i, k in enumerate(g.nodes)}, "label")
 
-        gjson = nx.json_graph.node_link_data(g)
-        schema = graphs.attributed_graph(g)
-        new_schema = plots.spring_force_graph(g)
+        
+        return g
+        
+    def test_spring_graph(self):
+        g = self.fake_network()
+        new_schema = plots.spring_force_graph(g, node_labels="label")
+
         self.assertEqual(
             len(vega.find_named(new_schema['data'], "node-data")['values']),  len(gjson["nodes"])
         )
@@ -102,28 +100,13 @@ class TestSpringGraph(unittest.TestCase):
             len(vega.find_named(new_schema['data'], "link-data")['values']),  len(gjson["links"])
         )
 
-    def test_edge_missing_attribute(self):
-        with self.assertRaises(ValueError):
-                g = nx.generators.barabasi_albert_graph(5, 3)
-                node_properties = {n: {"group": rand_group(), 
-                                    "fx": random.randint(1, 100),
-                                    "fy": random.randint(1, 100)}
-                                    for n in g.nodes()}
+    def test_spring_graph_label(self):
+        g = self.fake_network()
+        new_schema = plots.spring_force_graph(g, node_labels=None)
 
-                nx.set_node_attributes(g, node_properties)
-                nx.set_node_attributes(g, {k:f"n{i}" for i, k in enumerate(g.nodes)}, "label")
-
-                schema = graphs.spring_force_graph(g)
-
-
-    def test_node_missing_attribute(self):
-        with self.assertRaises(ValueError):
-                g = nx.generators.barabasi_albert_graph(5, 3)
-                node_properties = {n: {"label": self.rand_label()}
-                                    for n in g.nodes()}
-
-                edge_attributions = {e: {"attribution": self.rand_attributions()} for e in g.edges()}
-
-                nx.set_node_attributes(g, node_properties)
-                nx.set_edge_attributes(g, edge_attributions)
-                schema = graphs.spring_force_graph(g)
+        self.assertEqual(
+            len(vega.find_named(new_schema['data'], "node-data")['values']),  len(gjson["nodes"])
+        )
+        self.assertEqual(
+            len(vega.find_named(new_schema['data'], "link-data")['values']),  len(gjson["links"])
+        )
