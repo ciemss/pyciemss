ea181513dd9402bfa74518717b6d19dd4d3e8975
Sam Witty <samawitty@gmail.com>
2023-10-24 14:09:02 -0400

    remove pytest-cov

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 ./scripts/lint.sh
-pytest -s --cov=pyciemss/ --cov=test --cov-report=term-missing ${@-} --cov-report html
+pytest -s -n auto --cov=pyciemss/ --cov=test --cov-report=term-missing ${@-} --cov-report html
