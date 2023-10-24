c20e6657f236174e264494292949314888021749
Sam Witty <samawitty@gmail.com>
2023-10-24 11:18:52 -0400

    added types for interfaces, and deleted ODE implementations

--- src/pyciemss/interfaces.py
+++ src/pyciemss/interfaces.py
@@ -1,109 +1,95 @@
 import functools
-from typing import Iterable, Optional, TypeVar, Union
+from typing import Generic, Optional, TypeVar
 
 import pyro
 
-# Declare types
-# Note: this doesn't really do anything. More of a placeholder for how derived classes should be declared.
-Data = TypeVar("Data")
-Intervention = TypeVar("Intervention")
-InferredParameters = TypeVar("InferredParameters")
-State = TypeVar("State")
-Simulation = TypeVar("Simulation")
-Variable = TypeVar("Variable")
-ObjectiveFunction = TypeVar("ObjectiveFunction")
-Constraints = TypeVar("Constraints")
-OptimizationAlgorithm = TypeVar("OptimizationAlgorithm")
-OptimizationResult = TypeVar("OptimizationResult")
-Solution = TypeVar("Solution")
-
-# Set of default quantiles used by interface implementations
-DEFAULT_QUANTILES = [
-    0.01,
-    0.025,
-    0.05,
-    0.1,
-    0.15,
-    0.2,
-    0.25,
-    0.3,
-    0.35,
-    0.4,
-    0.45,
-    0.5,
-    0.55,
-    0.6,
-    0.65,
-    0.7,
-    0.75,
-    0.8,
-    0.85,
-    0.9,
-    0.95,
-    0.975,
-    0.99,
-]
+# By convention we use "T" to denote the type of the dynamical system, e.g. `ODE`, `PDE`, or `SDE`.
+T = TypeVar("T")
 
 
-# TODO: Figure out how to declare the parameteric type of `DynamicalSystem` in the signature.
-class DynamicalSystem(pyro.nn.PyroModule):
+class DynamicalSystem(Generic[T], pyro.nn.PyroModule):
     """
-    Base class for dynamical systems.
+    A dynamical system is a model of a system that evolves over time.
     """
 
-    def __init__(self) -> None:
-        super().__init__()
-        self.reset()
-
-    def reset(self):
-        raise NotImplementedError
-
-    def setup_before_solve(self):
-        raise NotImplementedError
-
-    def param_prior(self):
-        raise NotImplementedError
-
-    def get_solution(self, *args, **kwargs) -> Solution:
-        raise NotImplementedError
-
-    def add_observation_likelihoods(self, solution: Solution):
-        raise NotImplementedError
-
-    def log_solution(self, solution: Solution) -> Solution:
-        raise NotImplementedError
-
-    def forward(self, *args, **kwargs) -> Solution:
-        """
-        Joint distribution over model parameters, trajectories, and noisy observations.
-        """
-        # Setup the anything the dynamical system needs before solving.
-        self.setup_before_solve()
-
-        # Sample parameters from the prior
-        self.param_prior()
-
-        # Solve the ODE
-        solution = self.get_solution(*args, **kwargs)
-
-        # Add the observation likelihoods
-        self.add_observation_likelihoods(solution)
-
-        return self.log_solution(solution)
+    pass
 
 
-# TODO: Figure out how to declare the parameteric type of `DynamicalSystem` in the signature.
+class Intervention(Generic[T]):
+    """
+    An intervention is a change to a dynamical system that is not a change to the parameters.
+    """
+
+    pass
+
+
+class Data(Generic[T]):
+    """
+    Data is a collection of observations of the dynamical system.
+    """
+
+    pass
+
+
+class InferredParameters(Generic[T], pyro.nn.PyroModule):
+    """
+    InferredParameters are the parameters of the dynamical system that are inferred from data.
+    This will always be a pyro.nn.PyroModule, as we rely on Pyro's variational inference with AutoGuides.
+    """
+
+    pass
+
+
+class Simulation(Generic[T]):
+    """
+    A simulation is a collection of trajectories of a dynamical system.
+    """
+
+    pass
+
+
+class ObjectiveFunction(Generic[T]):
+    """
+    An objective function is a function that is optimized to infer parameters from data.
+    """
+
+    pass
+
+
+class Constraints(Generic[T]):
+    """
+    Constraints are constraints on the parameters of the dynamical system for optimization.
+    """
+
+    pass
+
+
+class OptimizationAlgorithm(Generic[T]):
+    """
+    An optimization algorithm is an algorithm that is used to optimize the objective function subject to constraints.
+    """
+
+    pass
+
+
+class OptimizationResult(Generic[T]):
+    """
+    An optimization result is the result of optimizing the objective function subject to constraints.
+    """
+
+    pass
+
+
 @functools.singledispatch
-def setup_model(model: DynamicalSystem, *args, **kwargs) -> DynamicalSystem:
+def setup_model(model: DynamicalSystem[T], *args, **kwargs) -> DynamicalSystem[T]:
     """
     Instatiate a model for a particular configuration of initial conditions, boundary conditions, logging events, etc.
     """
     raise NotImplementedError
 
 
-# TODO: Figure out how to declare the parameteric type of `DynamicalSystem` in the signature.
 @functools.singledispatch
-def reset_model(model: DynamicalSystem, *args, **kwargs) -> DynamicalSystem:
+def reset_model(model: DynamicalSystem[T], *args, **kwargs) -> DynamicalSystem[T]:
     """
     Reset a model to its initial state.
     reset_model * setup_model = id
@@ -111,11 +97,10 @@ def reset_model(model: DynamicalSystem, *args, **kwargs) -> DynamicalSystem:
     raise NotImplementedError
 
 
-# TODO: Figure out how to declare the parameteric type of `DynamicalSystem` in the signature.
 @functools.singledispatch
 def intervene(
-    model: DynamicalSystem, intervention: Intervention, *args, **kwargs
-) -> DynamicalSystem:
+    model: DynamicalSystem[T], intervention: Intervention[T], *args, **kwargs
+) -> DynamicalSystem[T]:
     """
     `intervene(model, intervention)` returns a new model where the intervention has been applied.
     """
@@ -123,81 +108,38 @@ def intervene(
 
 
 @functools.singledispatch
-def assert_observations_valid(
-    model: DynamicalSystem, data: Data, *args, **kwargs
-) -> None:
-    """
-    Check that the observations are valid for the given model.
-    """
-    raise NotImplementedError
-
-
-# TODO: Figure out how to declare the parameteric type of `DynamicalSystem` in the signature.
-@functools.singledispatch
 def calibrate(
-    model: DynamicalSystem, data: Data, *args, **kwargs
-) -> InferredParameters:
+    model: DynamicalSystem[T], data: Data[T], *args, **kwargs
+) -> InferredParameters[T]:
     """
     Infer parameters for a DynamicalSystem model conditional on data. This is typically done using a variational approximation.
     """
     raise NotImplementedError
 
 
-# TODO: Figure out how to declare the parameteric type of `DynamicalSystem` in the signature.
 @functools.singledispatch
-def sample(
-    model: DynamicalSystem,
-    inferred_parameters: Optional[InferredParameters] = None,
+def simulate(
+    model: DynamicalSystem[T],
+    inferred_parameters: Optional[InferredParameters[T]] = None,
     *args,
     **kwargs
-) -> Simulation:
+) -> Simulation[T]:
     """
-    Sample trajectories from a given `model`, conditional on specified `inferred_parameters`. If `inferred_parameters` is not given, this will sample from the prior distribution.
+    Simulate trajectories from a given `model`, conditional on specified `inferred_parameters` distribution. If `inferred_parameters` is not given, this will sample from the prior distribution.
     """
     raise NotImplementedError
 
 
-# TODO: Figure out how to declare the parameteric type of `DynamicalSystem` in the signature.
 @functools.singledispatch
 def optimize(
-    model: DynamicalSystem,
-    objective_function: ObjectiveFunction,
-    constraints: Constraints,
-    optimization_algorithm: OptimizationAlgorithm,
+    model: DynamicalSystem[T],
+    objective_function: ObjectiveFunction[T],
+    constraints: Constraints[T],
+    optimization_algorithm: OptimizationAlgorithm[T],
     *args,
     **kwargs
-) -> OptimizationResult:
+) -> OptimizationResult[T]:
     """
     Optimize the objective function subject to the constraints.
-    """
-    raise NotImplementedError
-
-
-@functools.singledispatch
-def prepare_interchange_dictionary(
-    samples: Simulation,
-    timepoints: Iterable[float],
-    time_unit: Optional[str],
-    alpha_qs: Optional[Iterable[float]],
-    stacking_order: Optional[str] = "timepoints",
-    visual_options: Union[None, bool, dict[str, any]] = None,
-    **kwargs
-) -> dict:
-    """
-    Reformat the internal representation of results for external consumption.
-    This may loose data, but it puts it into a convenient format for working with external tools
-    (and this method hsould be the _only_ place that loss is incurred for the purpose fo
-    interchange with other tools like terarium).
-
-    samples -- Output of the same shape as the 'sample' method.
-    timepoints -- The timepoints used to build the samples
-    time_unit -- Label for the timepoints (minutes, seconds, etc).
-                 It IS NOT interepreted or used in conversaionts
-    alpha_qs --
-    stacking_order -- Default is 'timepoints'
-    visual_otions -- If the implementation constructsion visaulizations, these are passed on.
-                    Set to None and no visuals are produced.  If True, visuals are produced
-                    with default options.
-    **kwargs -- Specific implementations may require additional information that may be passed here.
     """
     raise NotImplementedError
