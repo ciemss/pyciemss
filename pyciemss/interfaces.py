025b2ae7cabaaffb4f53a08f355f3b39b6c3b5b9
Sam Witty <samawitty@gmail.com>
2023-10-30 16:55:10 -0400

    add pyciemss_logging_wrapper and documentation

--- pyciemss/interfaces.py
+++ pyciemss/interfaces.py
@@ -1,5 +1,5 @@
 import contextlib
-from typing import Callable, Dict, Optional, Union
+from typing import Any, Callable, Dict, Optional, Union
 
 import pyro
 import torch
@@ -9,78 +9,74 @@ from chirho.dynamical.handlers import (
     LogTrajectory,
     StaticIntervention,
 )
+from chirho.dynamical.handlers.solver import TorchDiffEq
 from chirho.dynamical.ops import State
 
 from pyciemss.compiled_dynamics import CompiledDynamics
+from pyciemss.integration_utils.custom_decorators import pyciemss_logging_wrapper
 
 
+@pyciemss_logging_wrapper
 def sample(
     model_path_or_json: Union[str, Dict],
-    start_time: float,
     end_time: float,
     logging_step_size: float,
     num_samples: int,
     *,
+    solver_method="dopri5",
+    solver_options: Dict[str, Any] = {},
+    start_time: float = 0.0,
     inferred_parameters: Optional[pyro.nn.PyroModule] = None,
     static_interventions: Dict[float, Dict[str, torch.Tensor]] = {},
     dynamic_interventions: Dict[
-        Callable[[State[torch.Tensor]], torch.Tensor], Dict[str, torch.Tensor]
+        Callable[[Dict[str, torch.Tensor]], torch.Tensor], Dict[str, torch.Tensor]
     ] = {},
-) -> State[torch.Tensor]:
+) -> Dict[str, torch.Tensor]:
     """
     Load a model from a file, compile it into a probabilistic program, and sample from it.
 
     Args:
-        petri_model_or_path: Union[str, mira.metamodel.TemplateModel, mira.modeling.Model]
-            - A path to a petri net file, or a petri net object.
-            - This path can be a URL or a local path to a mira model or AMR model.
-            - Alternatively, this can be a mira template model directly.
+        model_path_or_json: Union[str, Dict]
+            - A path to a AMR model file or JSON containing a model in AMR form.
+        end_time: float
+            - The end time of the sampled simulation.
+        logging_step_size: float
+            - The step size to use for logging the trajectory.
         num_samples: int
             - The number of samples to draw from the model.
-        timepoints: [Iterable[float]]
-            - The timepoints to simulate the model from.
-              Backcasting and/or forecasting is reflected in the choice of timepoints.
         interventions: Optional[Iterable[Tuple[float, str, float]]]
             - A list of interventions to apply to the model.
               Each intervention is a tuple of the form (time, parameter_name, value).
-        start_state: Optional[dict[str, float]]
-            - The initial state of the model. If None, the initial state is taken from the mira model.
-        start_time: float
-            - The start time of the model. This is used to align the `start_state` with the `timepoints`.
-            - By default we set the `start_time` to be a small negative number to avoid numerical
-              issues w/ collision with the `timepoints` which typically start at 0.
-        method: str
+        solver_method: str
             - The method to use for solving the ODE. See torchdiffeq's `odeint` method for more details.
             - If performance is incredibly slow, we suggest using `euler` to debug.
               If using `euler` results in faster simulation, the issue is likely that the model is stiff.
-        compile_rate_law_p: bool
-            - Whether or not to compile the rate law from the AMR rate expression. default True.
-              False is useful for debugging.
-        compile_observables_p: bool
-            - Whether or not to compile the observables from the AMR observable expression. default True.
-              False is useful for debugging.
-        time_unit: str
-            - Time unit (used for labeling outputs)
-        visual_options: None, bool, dict[str, any]
-            - True output a visual
-            - False do not output a visual
-            - dict output a visual with the dictionary passed to the visualization as kwargs
-        alpha_qs: Optional[Iterable[float]]
-            - The quantiles required for estimating weighted interval score to test forecasting accuracy.
-        stacking_order: Optional[str]
-            - The stacking order requested for the quantiles to keep the selected quantity together for each state.
-            - Options: "timepoints" or "quantiles"
+        solver_options: Dict[str, Any]
+            - Options to pass to the solver. See torchdiffeq' `odeint` method for more details.
+        start_time: float
+            - The start time of the model. This is used to align the `start_state` from the
+              AMR model with the simulation timepoints.
+            - By default we set the `start_time` to be 0.
+        inferred_parameters:
+            - A Pyro module that contains the inferred parameters of the model.
+              This is typically the result of `calibrate`.
+            - If not provided, we will use the default values from the AMR model.
+        static_interventions: Dict[float, Dict[str, torch.Tensor]]
+            - A dictionary of static interventions to apply to the model.
+            - Each key is the time at which the intervention is applied.
+            - Each value is a dictionary of the form {state_variable_name: value}.
+        dynamic_interventions: Dict[Callable[[Dict[str, torch.Tensor]], torch.Tensor], Dict[str, torch.Tensor]]
+            - A dictionary of dynamic interventions to apply to the model.
+            - Each key is a function that takes in the current state of the model and returns a tensor.
+              When this function crosses 0, the dynamic intervention is applied.
+            - Each value is a dictionary of the form {state_variable_name: value}.
 
     Returns:
-        result: dict
-            - Dictionary of outputs with following attribute:
-                * data: The samples from the model as a pandas DataFrame.
-                * quantiles: The quantiles for ensemble score calculation as a pandas DataFrames.
-                * risk: Risk estimates for each state as 2-day average at the final timepoint
-                    * risk: Estimated alpha-superquantile risk with alpha=0.95
-                    * qoi: Samples of quantity of interest
-                          (in this case, 2-day average of the state at the final timepoint)
-                * visual: Visualization. (If visual_options is truthy)
+        result: Dict[str, torch.Tensor]
+            - Dictionary of outputs from the model.
+                - Each key is the name of a parameter or state variable in the model.
+                - Each value is a tensor of shape (num_samples, num_timepoints) for state variables
+                    and (num_samples,) for parameters.
     """
 
     model = CompiledDynamics.load(model_path_or_json)
@@ -104,7 +100,11 @@ def sample(
                         static_intervention_handlers + dynamic_intervention_handlers
                     ):
                         stack.enter_context(handler)
-                    model(torch.as_tensor(start_time), torch.tensor(end_time))
+                    model(
+                        torch.as_tensor(start_time),
+                        torch.tensor(end_time),
+                        TorchDiffEq(method=solver_method, options=solver_options),
+                    )
         # Adding deterministic nodes to the model so that we can access the trajectory in the Predictive object.
         [pyro.deterministic(f"state_{k}", v) for k, v in lt.trajectory.items()]
 
