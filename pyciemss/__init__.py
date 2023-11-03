6887529d6c03d463aa731e60d8fd37f9edb078fa
Five Grant <5@fivegrant.com>
2023-11-03 08:37:57 -0500

    Remove package finding AND add to module exports (#407)
    * Remove package finding AND add to module exports
    
    * Clear out top-level `__init__.py`
    
    * Reinclude explicit exports
    
    * Include subpackages
    
    * Skip incorrect lint error
    
    * reorder imports for lint
    
    * revert previous commit and add missing space before noqa
    
    ---------
    
    Co-authored-by: Sam Witty <samawitty@gmail.com>

--- pyciemss/__init__.py
+++ pyciemss/__init__.py
@@ -1,1 +1,2 @@
-from .mira_integration import compiled_dynamics  # noqa: F401
+from pyciemss.interfaces import sample  # noqa: F401
+from pyciemss.mira_integration import compiled_dynamics  # noqa: F401
