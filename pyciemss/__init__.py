d87efdde72d5addf3dbe6a9a8b6efde33594316c
Sam Witty <samawitty@gmail.com>
2023-11-02 22:45:00 -0400

    revert previous commit and add missing space before noqa

--- pyciemss/__init__.py
+++ pyciemss/__init__.py
@@ -1,2 +1,2 @@
-from pyciemss.mira_integration import compiled_dynamics
-from pyciemss.interfaces import sample
+from pyciemss.interfaces import sample  # noqa: F401
+from pyciemss.mira_integration import compiled_dynamics  # noqa: F401
