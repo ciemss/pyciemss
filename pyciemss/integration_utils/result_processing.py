73d21346c2f96b68a2a2a88aa3b488ba3e611fc7
Sam Witty <samawitty@gmail.com>
2023-11-06 10:11:00 -0500

    Add basic output formatting (#405)
    * initial pass at output processing
    
    * lint
    
    * add pandas and numpy dependencies
    
    * remove outdated docstring
    
    * Remove package finding AND add to module exports (#407) (#409)
    
    * Remove package finding AND add to module exports
    
    * Clear out top-level `__init__.py`
    
    * Reinclude explicit exports
    
    * Include subpackages
    
    * Skip incorrect lint error
    
    * reorder imports for lint
    
    * revert previous commit and add missing space before noqa
    
    ---------
    
    Co-authored-by: Five Grant <5@fivegrant.com>
    
    ---------
    
    Co-authored-by: Five Grant <5@fivegrant.com>

---
+++ pyciemss/integration_utils/result_processing.py
@@ -0,0 +1,66 @@
+from typing import Any, Dict
+
+import numpy as np
+import pandas as pd
+import torch
+
+
+def prepare_interchange_dictionary(
+    samples: Dict[str, torch.Tensor],
+) -> Dict[str, Any]:
+    processed_samples = convert_to_output_format(samples)
+
+    result = {"data": processed_samples, "unprocessed_result": samples}
+
+    return result
+
+
+def convert_to_output_format(samples: Dict[str, torch.Tensor]) -> pd.DataFrame:
+    """
+    Convert the samples from the Pyro model to a DataFrame in the TA4 requested format.
+    """
+
+    pyciemss_results: Dict[str, Dict[str, torch.Tensor]] = {
+        "parameters": {},
+        "states": {},
+    }
+
+    for name, sample in samples.items():
+        if sample.ndim == 1:
+            # Any 1D array is a sample from the distribution over parameters.
+            # Any 2D array is a sample from the distribution over states, unless it's a model weight.
+            name = name + "_param"
+            pyciemss_results["parameters"][name] = (
+                sample.data.detach().cpu().numpy().astype(np.float64)
+            )
+        else:
+            pyciemss_results["states"][name] = (
+                sample.data.detach().cpu().numpy().astype(np.float64)
+            )
+
+    num_samples, num_timepoints = next(iter(pyciemss_results["states"].values())).shape
+    output = {
+        "timepoint_id": np.tile(np.array(range(num_timepoints)), num_samples),
+        "sample_id": np.repeat(np.array(range(num_samples)), num_timepoints),
+    }
+
+    # Parameters
+    output = {
+        **output,
+        **{
+            k: np.repeat(v, num_timepoints)
+            for k, v in pyciemss_results["parameters"].items()
+        },
+    }
+
+    # Solution (state variables)
+    output = {
+        **output,
+        **{
+            k: np.squeeze(v.reshape((num_timepoints * num_samples, 1)))
+            for k, v in pyciemss_results["states"].items()
+        },
+    }
+
+    result = pd.DataFrame(output)
+    return result
