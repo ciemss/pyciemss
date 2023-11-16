833b2040f6fcf49ce66a44061d2408e34a174721
Sam Witty <samawitty@gmail.com>
2023-11-16 14:12:59 -0500

    Add basic integration notebook and CI for testing notebooks (#416)
    * add basic notebook and nbval test
    
    * nbval CI
    
    * make ensemble a module
    
    * changed imports in CI builds and added imports throughout
    
    * reran interfaces
    
    * lint

--- pyciemss/__init__.py
+++ pyciemss/__init__.py
@@ -1,2 +1,3 @@
-from pyciemss.interfaces import sample  # noqa: F401
-from pyciemss.mira_integration import compiled_dynamics  # noqa: F401
+import pyciemss.compiled_dynamics  # noqa: F401
+import pyciemss.mira_integration  # noqa: F401
+from pyciemss.interfaces import ensemble_sample, sample  # noqa: F401
