416bd638c95ad86ed221a8be06fdc4c11ff267d7
sabinala <130604122+sabinala@users.noreply.github.com>
2024-01-19 17:59:52 -0800

    adding check on num_samples (#455)
    * adding check on num_samples
    
    * reformatting
    
    * Update interfaces.py
    
    Changing assert statement to if (condition) raise ValueError
    
    * adding unit tests for num_samples in sample and ensemble sample
    
    * moving unit tests to their own files

--- pyciemss/interfaces.py
+++ pyciemss/interfaces.py
@@ -101,6 +101,10 @@ def ensemble_sample(
 
     timespan = torch.arange(start_time + logging_step_size, end_time, logging_step_size)
 
+    # Check that num_samples is a positive integer
+    if not (isinstance(num_samples, int) and num_samples > 0):
+        raise ValueError("num_samples must be a positive integer")
+
     def wrapped_model():
         # We need to interleave the LogTrajectory and the solutions from the models.
         # This because each contituent model will have its own LogTrajectory.
@@ -238,6 +242,10 @@ def sample(
     model = CompiledDynamics.load(model_path_or_json)
 
     timespan = torch.arange(start_time + logging_step_size, end_time, logging_step_size)
+
+    # Check that num_samples is a positive integer
+    if not (isinstance(num_samples, int) and num_samples > 0):
+        raise ValueError("num_samples must be a positive integer")
 
     static_state_intervention_handlers = [
         StaticIntervention(time, dict(**static_intervention_assignment))
