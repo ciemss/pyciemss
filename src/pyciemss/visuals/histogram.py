7ddcef029745ff753b0bbb34155b3e84253833d7
Joseph Cottam <joseph.cottam@pnnl.gov>
2024-01-24 15:24:26 -0800

    Removing things from old locations.

---
+++ src/pyciemss/visuals/histogram.py
@@ -0,0 +1,171 @@
+from typing import List, Callable, Optional, Tuple
+
+from numbers import Number
+
+import pandas as pd
+import numpy as np
+
+from . import vega
+
+
+def sturges_bin(data):
+    """Determine number of bin susing sturge's rule.
+    TODO: Consider Freedman-Diaconis (larger data sizes and spreads)
+    """
+    return int(np.ceil(np.log2(len(data))) + 1)
+
+
+def histogram_multi(
+    *,
+    xrefs: List[Number] = [],
+    yrefs: List[Number] = [],
+    bin_rule: Callable = sturges_bin,
+    return_bins: bool = False,
+    **data,
+) -> vega.VegaSchema:
+    """
+    Create a histogram with server-side binning.
+
+    TODO: Maybe compute overlap in bins explicitly and visualize as stacked?
+          Limits (practically) to two distributions, but legend is more clear.
+
+    TODO: Need to align histogram bin size between groups to make the visual
+          representation more interpretable
+
+    **data -- Datasets, name will be used to build labels.
+    bin_rule -- Determines bins width using this function.
+                Will received a joint dataframe of all data passed
+    xrefs - List of values in the bin-range to highlight as vertical lines
+    yrefs - List of values in the count-range to highlight as horizontal lines
+    bins - Number of bins to divide into
+    """
+
+    schema = vega.load_schema("histogram_static_bins_multi.vg.json")
+
+    def hist(label, subset, edges):
+        assignments = np.digitize(subset, edges) - 1
+        counts = np.bincount(assignments)
+        spans = [*(zip(edges, edges[1:]))]
+        desc = [
+            {"bin0": l.item(), "bin1": h.item(), "count": c.item(), "label": label}
+            for ((l, h), c) in zip(spans, counts)
+        ]
+        return desc
+
+    def as_value_list(label, data):
+        try:
+            return data["state_values"].rename(label)
+        except BaseException:
+            return pd.Series(data).rename(label)
+
+    data = {k: as_value_list(k, subset) for k, subset in data.items()}
+
+    joint = pd.concat(data)
+    bins_count = bin_rule(joint)
+    _, edges = np.histogram(joint, bins=bins_count)
+
+    hists = {k: hist(k, subset, edges) for k, subset in data.items()}
+    desc = [item for sublist in hists.values() for item in sublist]
+
+    schema["data"] = vega.replace_named_with(schema["data"], "binned", ["values"], desc)
+
+    schema["data"] = vega.replace_named_with(
+        schema["data"], "xref", ["values"], [{"value": v} for v in xrefs]
+    )
+
+    schema["data"] = vega.replace_named_with(
+        schema["data"], "yref", ["values"], [{"count": v} for v in yrefs]
+    )
+
+    if return_bins:
+        return schema, pd.DataFrame(desc).set_index(["bin0", "bin1"])
+    else:
+        return schema
+
+
+def heatmap_scatter(
+    points: pd.DataFrame,
+    mesh: Optional[Tuple] = None,
+    *,
+    x_name: Optional[str] = None,
+    y_name: Optional[str] = None,
+    max_x_bins: int = 10,
+    max_y_bins: int = 10,
+) -> vega.VegaSchema:
+    """
+    Render a heatmap with points overlaid.
+
+    The heatmap can be IMPLICIT and derived from the points or it can EXPLICIT
+    and supplied as 'mesh'. If it implicit, then a binning function is used at
+    render time that does bin-boundry 'nciing.  If specific bin-count or bin-boundries
+    are needed, use an explicit mesh.
+
+    points -- Set of points to render as a scatterplot.
+    mesh -- (Optional) Tripple of x-indicies, y-indicies and values.
+        `x-indicies` and `y-indicies` as-produced by numpy meshgrid.
+        `values` as a numpy array with shape compatible with x- and y-indicies
+    x_name: str name of column in dataset for x axis, (defaults to first column)
+    y_name: str, name of column in dataset for y axis (defaults to second column)
+    max_x_bins: int = 10, maximum number bins for the x axis (ignored for explicit mesh)
+    max_y_bins: int = 10, maximum number bins for the y axis (ignored for explicit mesh)
+    """
+
+    def _mesh_to_heatmap(mesh_data):
+        """
+        **mesh_data -- input as mesh data, will be converted to grids
+        adding half the difference in grid spacing to each coordinate
+        so point becomes center of a grid for heatmap
+        """
+        xv, yv, zz = mesh_data
+        half_spacing_x = (xv[0, 1] - xv[0, 0]) / 2
+        half_spacing_y = (yv[1, 0] - yv[0, 0]) / 2
+        dataset = pd.DataFrame(
+            {
+                "x_start": xv.ravel() - half_spacing_x,
+                "x_end": xv.ravel() + half_spacing_x,
+                "y_start": yv.ravel() - half_spacing_y,
+                "y_end": yv.ravel() + half_spacing_y,
+                "__count": zz.ravel(),
+            }
+        )
+        return dataset.to_dict(orient="records")
+
+    json_points = points.to_dict(orient="records")
+
+    if x_name is None:
+        x_name = points.columns[0]
+    if y_name is None:
+        y_name = points.columns[1]
+
+    if mesh is None:
+        schema = vega.load_schema("heatmap_scatter.vg.json")
+
+        schema["data"] = vega.replace_named_with(
+            schema["data"], "points", ["values"], json_points
+        )
+
+        schema["signals"] = vega.replace_named_with(
+            schema["signals"], "max_x_bins", ["value"], max_x_bins
+        )
+        schema["signals"] = vega.replace_named_with(
+            schema["signals"], "max_y_bins", ["value"], max_y_bins
+        )
+
+        schema["signals"] = vega.replace_named_with(
+            schema["signals"], "x_name", ["value"], x_name
+        )
+
+        schema["signals"] = vega.replace_named_with(
+            schema["signals"], "y_name", ["value"], y_name
+        )
+    else:
+        schema = vega.load_schema("mesh_scatter.vg.json")
+        json_heatmap = _mesh_to_heatmap(mesh)
+        schema["data"] = vega.replace_named_with(
+            schema["data"], "points", ["values"], json_points
+        )
+        schema["data"] = vega.replace_named_with(
+            schema["data"], "mesh", ["values"], json_heatmap
+        )
+
+    return schema
---
+++ src/pyciemss/visuals/plots.py
@@ -0,0 +1,93 @@
+from typing import Dict, Any, Literal, Optional
+
+import json
+
+import IPython.display
+import vl_convert
+
+from .vega import VegaSchema, pad, resize, set_title, rescale, orient_legend
+from .barycenter import triangle_contour
+from .trajectories import trajectories
+from .calibration import calibration
+from .histogram import histogram_multi, heatmap_scatter
+from .graphs import attributed_graph, spring_force_graph
+
+
+__all__ = [
+    VegaSchema,
+    pad,
+    resize,
+    set_title,
+    rescale,
+    orient_legend,
+    triangle_contour,
+    trajectories,
+    calibration,
+    histogram_multi,
+    attributed_graph,
+    spring_force_graph,
+    heatmap_scatter,
+]
+
+
+def save_schema(schema: Dict[str, Any], path: str):
+    """Save the schema using common convention"""
+    with open(path, "w") as f:
+        json.dump(schema, f, indent=3)
+
+
+def ipy_display(
+    schema: Dict[str, Any],
+    *,
+    format: Literal["PNG", "SVG", "interative"] = "PNG",
+    force_clear: bool = False,
+    dpi: Optional[int] = None,
+    **kwargs,
+):
+    """Wrap for dispaly in an ipython notebook.
+    schema -- A vega JSON schema ready for rendering
+    format -- Return a PNG or SVG if requested (wrapped for jupyter dipslay)
+                OR for "interactive" returns None but displays the schema
+              Format specifier is case-insensitive.
+    force_clear -- Force clear the result cell (sometimes required after an error)
+    dpi -- approximates DPI for output (other factors apply)
+    **kwargs -- Passed on to the selected vl_convert function
+
+    The vlc_convert PNG export function takes a 'scale' factor,
+    which uses in machine-dependent resolution units.
+    If dpi is specified and kwargs DOES NOT include a scale,
+    then dpi will be used to estimate the scale factor to get a result near the requested
+    resolution assuming a machine-resolution of 72dpi.
+
+    This (may) return PNG data **wrappped** for display in jupyter.
+    The raw PNG data is accessible from the returned objects `data` property. Save to a file as:
+    ```
+    image = display(schema)
+    with open("test_image.png", "wb") as f:
+       f.write(image.data)
+    ```
+
+    return -- PNG data to display (if not interactive) OR an interactive vega plot
+
+    """
+    if force_clear:
+        IPython.display.clear_output(wait=True)
+
+    format = format.lower()
+
+    if format == "interactive":
+        bundle = {"application/vnd.vega.v5+json": schema}
+
+        print("", end=None)
+        IPython.display.display(bundle, raw=True)
+    elif format == "png":
+        if dpi and "scale" not in kwargs:
+            kwargs["scale"] = dpi // 72
+
+        png_data = vl_convert.vega_to_png(schema, **kwargs)
+        return IPython.display.Image(png_data)
+    elif format == "svg":
+        png_data = vl_convert.vega_to_svg(schema, **kwargs)
+        return IPython.display.SVG(png_data)
+    else:
+        raise ValueError(f"Unhandled format requested: {format}")
