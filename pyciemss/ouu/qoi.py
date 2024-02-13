419d740275e776fdb03e1704256ec692ac736081
Anirban Chaudhuri <75496534+anirban-chaudhuri@users.noreply.github.com>
2024-02-13 14:07:52 -0500

    simplify qoi def

--- pyciemss/ouu/qoi.py
+++ pyciemss/ouu/qoi.py
@@ -1,16 +1,18 @@
-from typing import List
+from typing import Dict, List
 
 import numpy as np
+import torch
 
 
-def obs_nday_average_qoi(dataCube, contexts: List, ndays: int = 7) -> np.ndarray:
+def obs_nday_average_qoi(
+    samples: Dict[str, torch.Tensor], contexts: List, ndays: int = 7
+) -> np.ndarray:
     """
     Return estimate of last n-day average of each sample.
-    dataCube is is the output from a Pyro Predictive object.
-    dataCube[VARIABLE] is expected to have dimension (nreplicates, ntimepoints)
+    samples is is the output from a Pyro Predictive object.
+    samples[VARIABLE] is expected to have dimension (nreplicates, ntimepoints)
     Note: last ndays timepoints is assumed to represent last n-days of simulation.
     """
-    if contexts is not None:
-        dataQoI = dataCube[contexts[0]].detach().numpy()
+    dataQoI = samples[contexts[0]].detach().numpy()
 
     return np.mean(dataQoI[:, -ndays:], axis=1)
