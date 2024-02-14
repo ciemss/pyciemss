f5de2acd6db3411acd8ad19089313ec56d30ea82
Joseph Cottam <joseph.cottam@pnnl.gov>
2024-02-14 10:51:59 -0800

    Converted to model tests to pytest

--- tests/test_model_representations.py
+++ tests/test_model_representations.py
@@ -1,5 +1,4 @@
-import unittest
-
+import pytest
 import requests
 from askem_model_representations import model_inventory
 
@@ -19,47 +18,27 @@ def simplify_inventory(inventory):
     return inventory
 
 
-class TestModelRepresentations(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        # Fetch model files.
-        # NOTE: If 'discovery' is required, look at the python 'git' module
-        repo_root = (
-            "https://raw.githubusercontent.com/DARPA-ASKEM/Model-Representations/main"
-        )
-        cls.model_files = [
-            f"{repo_root}/petrinet/examples/flux_typed.json",
-            f"{repo_root}/petrinet/examples/flux_typed_aug.json",
-            # "{repo_root}/halfar.json",
-            # "{repo_root}/on_pop_vax.json",
-            # "{repo_root}/sir.json",
-            f"{repo_root}/petrinet/examples/sir_flux_span.json",
-            # "{repo_root}/sir_typed.json",
-            # "{repo_root}/sir_typed_aug.json",
-            f"{repo_root}/regnet/examples/lotka_volterra.json",
-            f"{repo_root}/stockflow/examples/sir.json",
-        ]
+repo_root = "https://raw.githubusercontent.com/DARPA-ASKEM/Model-Representations/main"
+model_files = [
+    f"{repo_root}/petrinet/examples/flux_typed.json",
+    f"{repo_root}/petrinet/examples/flux_typed_aug.json",
+    # "{repo_root}/halfar.json",
+    # "{repo_root}/on_pop_vax.json",
+    # "{repo_root}/sir.json",
+    f"{repo_root}/petrinet/examples/sir_flux_span.json",
+    # "{repo_root}/sir_typed.json",
+    # "{repo_root}/sir_typed_aug.json",
+    f"{repo_root}/regnet/examples/lotka_volterra.json",
+    f"{repo_root}/stockflow/examples/sir.json",
+]
 
-        cls.models = {}
-        for file in cls.model_files:
-            try:
-                cls.models[file] = requests.get(file).json()
-            except Exception:
-                unittest.skip("Could not fetch all requsted model files")
-                # raise ValueError(file)
-                pass
 
-    def test_representations(self):
-        self.assertEqual(
-            len(self.models),
-            len(self.model_files),
-            "Not all models loaded as expected.",
-        )
+@pytest.mark.parametrize("model_file", model_files)
+def test_representations(model_file):
+    try:
+        model = requests.get(model_file).json()
+    except BaseException:
+        assert False, f"Could not load model {model_file}"
+    inventory = model_inventory.check_amr(model, summary=True)
 
-        issues = {}
-        for model_name, model in self.models.items():
-            inventory = model_inventory.check_amr(model, summary=True)
-            if not is_satisfactory(inventory):
-                issues[model_name] = inventory
-
-        self.assertFalse(issues, f"{len(issues)} issues found")
+    assert is_satisfactory(inventory), f"{model_file} not satisfactory"
