f019107cc88bfba824300e0baea5203669863504
Sam Witty <samawitty@gmail.com>
2024-03-11 16:39:32 -0400

    add additional test to ensure that parameter intervention isn't resetting state with noop intervention

--- tests/test_interruptions.py
+++ tests/test_interruptions.py
@@ -122,3 +122,51 @@ def test_parameter_intervention_after_end(
     check_is_state(simulation1, torch.Tensor)
     check_is_state(simulation2, torch.Tensor)
     check_states_match(simulation1, simulation2)
+
+@pytest.mark.parametrize("model_fixture", MODELS)
+@pytest.mark.parametrize("start_time", START_TIMES)
+@pytest.mark.parametrize("end_time", END_TIMES)
+@pytest.mark.parametrize("intervention_handler_type", INTERVENTION_HANDLER_TYPES)
+def test_parameter_intervention_noop(
+    model_fixture,
+    start_time,
+    end_time,
+    intervention_handler_type,
+):
+    model = CompiledDynamics.load(model_fixture.url)
+    assert isinstance(model, CompiledDynamics)
+
+    intervention_time = start_time + (end_time - start_time) / 2.0
+    parameter = model_fixture.important_parameter
+    intervention = {parameter: lambda x : x}
+
+    if intervention_handler_type == "static":
+        intervention_handler = StaticParameterIntervention(
+            time=intervention_time, intervention=intervention
+        )
+    elif intervention_handler_type == "dynamic":
+
+        def event_fn(time: torch.Tensor, *args, **kwargs) -> torch.Tensor:
+            return time - intervention_time
+
+        intervention_handler = DynamicParameterIntervention(
+            event_fn=event_fn, intervention=intervention
+        )
+
+    if parameter is None:
+        pytest.skip("Model does not have an important parameter.")
+
+    with pyro.poutine.seed(rng_seed=0):
+        with TorchDiffEq():
+            with intervention_handler:
+                simulation1 = model(
+                    torch.as_tensor(start_time), torch.as_tensor(end_time)
+                )
+
+    with pyro.poutine.seed(rng_seed=0):
+        with TorchDiffEq():
+            simulation2 = model(torch.as_tensor(start_time), torch.as_tensor(end_time))
+
+    check_is_state(simulation1, torch.Tensor)
+    check_is_state(simulation2, torch.Tensor)
+    check_states_match(simulation1, simulation2)
