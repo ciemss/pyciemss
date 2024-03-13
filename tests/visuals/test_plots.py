5cde5fd056b4735f318c6d6ed242ec027f8ef133
marjoleinpnnl <87781337+marjoleinpnnl@users.noreply.github.com>
2024-03-13 16:39:03 -0700

    507 add reference lines to trajectory plot (#521)
    * add trajectory horizonal to vg file
    * add markers to test
    * update trajectory plot
    * correcting names

--- tests/visuals/test_plots.py
+++ tests/visuals/test_plots.py
@@ -83,6 +83,16 @@ class TestTrajectory:
         df = pd.DataFrame(vega.find_named(schema["data"], "distributions")["values"])
         assert {"trajectory", "timepoint", "lower", "upper"} == set(df.columns)
 
+    def test_markers(self, distributions):
+        schema = plots.trajectories(
+            distributions,
+            base_markers_h={"Low marker": 10000000, "High marker": 20000000},
+        )
+
+        plots.save_schema(schema, "test_markers_schema.vg.json")
+        df = pd.DataFrame(vega.find_named(schema["data"], "markers_h")["values"])
+        assert {"axis_value", "label"} == set(df.columns)
+
     def test_rename(self, distributions):
         nice_labels = make_nice_labels(distributions.columns)
 
