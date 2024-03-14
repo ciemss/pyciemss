b5ed105ee58847ca17dda35ec203e1f80323ea0d
sabinala <130604122+sabinala@users.noreply.github.com>
2024-03-14 12:32:23 -0700

    adding ensemble calibrate example (#537)
    * adding ensemble calibrate example
    
    * add ensemble_calibrate to main
    
    * lint
    
    * removing from pyciemss import
    
    * removing from pyciemss import
    
    * remove commented out line
    
    ---------
    
    Co-authored-by: Sam Witty <samawitty@gmail.com>

--- pyciemss/__init__.py
+++ pyciemss/__init__.py
@@ -2,6 +2,7 @@ import pyciemss.compiled_dynamics  # noqa: F401
 import pyciemss.mira_integration  # noqa: F401
 from pyciemss.interfaces import (  # noqa: F401
     calibrate,
+    ensemble_calibrate,
     ensemble_sample,
     optimize,
     sample,
