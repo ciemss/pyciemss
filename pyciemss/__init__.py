33ac5a7cb1486c5dedbcb43835d53b2d3b2c9399
Sam Witty <samawitty@gmail.com>
2024-03-14 14:15:46 -0400

    lint

--- pyciemss/__init__.py
+++ pyciemss/__init__.py
@@ -2,8 +2,8 @@ import pyciemss.compiled_dynamics  # noqa: F401
 import pyciemss.mira_integration  # noqa: F401
 from pyciemss.interfaces import (  # noqa: F401
     calibrate,
-    ensemble_sample,
     ensemble_calibrate,
+    ensemble_sample,
     optimize,
     sample,
 )
