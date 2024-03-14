c169bf4bba07e1191b4cfe40c9ae573f74b0b947
Sam Witty <samawitty@gmail.com>
2024-03-14 14:15:28 -0400

    add ensemble_calibrate to main

--- pyciemss/__init__.py
+++ pyciemss/__init__.py
@@ -3,6 +3,7 @@ import pyciemss.mira_integration  # noqa: F401
 from pyciemss.interfaces import (  # noqa: F401
     calibrate,
     ensemble_sample,
+    ensemble_calibrate,
     optimize,
     sample,
 )
