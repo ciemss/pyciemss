e3bdd969d371015abb4df172b6a6885e075f51b2
Sam Witty <samawitty@gmail.com>
2024-03-14 11:59:35 -0400

    Robustify notebook test CI to allow notebook outputs (#536)
    * add notebook with outputs
    
    * clear notebook outputs before running test in CI
    
    Resolving merge conflicts

--- scripts/test_notebooks.sh
+++ scripts/test_notebooks.sh
@@ -2,4 +2,5 @@
 
 INCLUDED_NOTEBOOKS="docs/source/*.ipynb"
 
+jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace --to notebook $INCLUDED_NOTEBOOKS
 CI=1 pytest --nbval-lax $INCLUDED_NOTEBOOKS
