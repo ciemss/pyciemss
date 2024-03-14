23261b4c4166cf351bbf240f56412bd7faa226df
Oostrom, Marjolein T <marjolein.oostrom@pnnl.gov>
2024-03-14 16:00:35 -0700

    use different step sizes

--- tests/visuals/test_plots.py
+++ tests/visuals/test_plots.py
@@ -42,7 +42,7 @@ def create_distributions(logging_step_size=20, time_unit="twenty"):
         time_unit=time_unit,
         start_time=start_time,
         solver_method="euler",
-        solver_options={"step_size": logging_step_size},
+        solver_options={"step_size": 1e-2},
     )["unprocessed_result"]
 
     return convert_to_output_format(
