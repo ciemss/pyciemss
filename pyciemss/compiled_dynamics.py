9266c28849108c6f7f794e9ae2f81cc11fc441e8
Sam Witty <samawitty@gmail.com>
2024-03-21 21:21:09 -0400

    Add more informative error if adaptive time step underflows (#549)
    * add additional description for underflow
    
    * lint

--- pyciemss/compiled_dynamics.py
+++ pyciemss/compiled_dynamics.py
@@ -74,7 +74,19 @@ class CompiledDynamics(pyro.nn.PyroModule):
 
         if logging_times is not None:
             with LogTrajectory(logging_times) as lt:
-                simulate(self.deriv, self.initial_state(), start_time, end_time)
+                try:
+                    simulate(self.deriv, self.initial_state(), start_time, end_time)
+                except AssertionError as e:
+                    if str(e) == "AssertionError: underflow in dt nan":
+                        raise AssertionError(
+                            "Underflow in the adaptive time step size. "
+                            "This is likely due to a stiff system of ODEs. "
+                            "Try changing the (distribution on) parameters, the rate laws, "
+                            "the initial state, or the time span."
+                        ) from e
+                    else:
+                        raise e
+
                 state = lt.trajectory
         else:
             state = simulate(self.deriv, self.initial_state(), start_time, end_time)
