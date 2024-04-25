161f9d9bcbcd57a2f5a377f0f6945edc5dc2840e
Sam Witty <samawitty@gmail.com>
2024-04-25 11:28:56 -0400

    replaced DEFAULT_ALPHA_QS with import

--- pyciemss/interfaces.py
+++ pyciemss/interfaces.py
@@ -33,31 +33,7 @@ from pyciemss.ouu.risk_measures import alpha_superquantile
 
 warnings.simplefilter("always", UserWarning)
 
-DEFAULT_ALPHA_QS = [
-    0.01,
-    0.025,
-    0.05,
-    0.1,
-    0.15,
-    0.2,
-    0.25,
-    0.3,
-    0.35,
-    0.4,
-    0.45,
-    0.5,
-    0.55,
-    0.6,
-    0.65,
-    0.7,
-    0.75,
-    0.8,
-    0.85,
-    0.9,
-    0.95,
-    0.975,
-    0.99,
-]
+from pyciemss.integration_utils.result_processing import DEFAULT_ALPHA_QS
 
 
 @pyciemss_logging_wrapper
