f65e111dff37c2521599265dc18b851b99dbdcb1
Sam Witty <samawitty@gmail.com>
2024-07-24 13:23:00 -0400

    lint

--- pyciemss/mira_integration/distributions.py
+++ pyciemss/mira_integration/distributions.py
@@ -24,6 +24,7 @@ def mira_normal_to_pyro(
 
     return pyro.distributions.Normal(loc, scale)
 
+
 def mira_lognormal_to_pyro(
     parameters: Dict[str, float]
 ) -> pyro.distributions.Distribution:
@@ -35,6 +36,7 @@ def mira_lognormal_to_pyro(
         scale = parameters["varLog"] ** 0.5
 
     return pyro.distributions.LogNormal(loc, scale)
+
 
 # Provide either probs or logits, not both
 def mira_bernoulli_to_pyro(
@@ -49,15 +51,18 @@ def mira_bernoulli_to_pyro(
 
     return pyro.distributions.Bernoulli(probs, logits)
 
-def mira_beta_to_pyro(
-    parameters: Dict[str, float]
-) -> pyro.distributions.Distribution:
+
+def mira_beta_to_pyro(parameters: Dict[str, float]) -> pyro.distributions.Distribution:
     return pyro.distributions.Beta(parameters["alpha"], parameters["beta"])
+
 
 def mira_betabinomial_to_pyro(
     parameters: Dict[str, Union[float, list]]
 ) -> pyro.distributions.Distribution:
-    return pyro.distributions.BetaBinomial(parameters["alpha"], parameters["beta"], parameters["numberOfTrials"])
+    return pyro.distributions.BetaBinomial(
+        parameters["alpha"], parameters["beta"], parameters["numberOfTrials"]
+    )
+
 
 def mira_binomial_to_pyro(
     parameters: Dict[str, Union[float, list]]
@@ -72,20 +77,24 @@ def mira_binomial_to_pyro(
 
     return pyro.distributions.Binomial(total_count, probs, logits)
 
+
 def mira_cauchy_to_pyro(
     parameters: Dict[str, float]
 ) -> pyro.distributions.Distribution:
     return pyro.distributions.Cauchy(parameters["location"], parameters["scale"])
+
 
 def mira_chisquared_to_pyro(
     parameters: Dict[str, float]
 ) -> pyro.distributions.Distribution:
     return pyro.distributions.Chi2(parameters["degreesOfFreedom"])
 
+
 def mira_dirichlet_to_pyro(
     parameters: Dict[str, list]
 ) -> pyro.distributions.Distribution:
     return pyro.distributions.Dirichlet(parameters["concentration"])
+
 
 def mira_exponential_to_pyro(
     parameters: Dict[str, float]
@@ -96,9 +105,8 @@ def mira_exponential_to_pyro(
         rate = 1.0 / parameters["mean"]
     return pyro.distributions.Exponential(rate)
 
-def mira_gamma_to_pyro(
-    parameters: Dict[str, float]
-) -> pyro.distributions.Distribution:
+
+def mira_gamma_to_pyro(parameters: Dict[str, float]) -> pyro.distributions.Distribution:
     if "shape" in parameters.keys():
         concentration = parameters["shape"]
     if "scale" in parameters.keys():
@@ -107,20 +115,23 @@ def mira_gamma_to_pyro(
         rate = parameters["rate"]
     return pyro.distributions.Gamma(concentration, rate)
 
+
 def mira_inversegamma_to_pyro(
     parameters: Dict[str, float]
 ) -> pyro.distributions.Distribution:
     return pyro.distributions.Gamma(parameters["shape"], parameters["scale"])
+
 
 def mira_gumbel_to_pyro(
     parameters: Dict[str, float]
 ) -> pyro.distributions.Distribution:
     return pyro.distributions.Gumbel(parameters["location"], parameters["scale"])
 
+
 def mira_laplace_to_pyro(
     parameters: Dict[str, float]
 ) -> pyro.distributions.Distribution:
-    
+
     if "location" in parameters.keys():
         loc = parameters["location"]
     elif "mu" in parameters.keys():
@@ -133,15 +144,18 @@ def mira_laplace_to_pyro(
 
     return pyro.distributions.Laplace(loc, scale)
 
+
 def mira_paretotypeI_to_pyro(
     parameters: Dict[str, float]
 ) -> pyro.distributions.Distribution:
     return pyro.distributions.Pareto(parameters["scale"], parameters["shape"])
 
+
 def mira_poisson_to_pyro(
     parameters: Dict[str, float]
 ) -> pyro.distributions.Distribution:
     return pyro.distributions.Poisson(parameters["rate"])
+
 
 def mira_studentt_to_pyro(
     parameters: Dict[str, float]
@@ -159,6 +173,7 @@ def mira_studentt_to_pyro(
         scale = 1.0
 
     return pyro.distributions.StudentT(parameters["degreesOfFreedom"], loc, scale)
+
 
 def mira_weibull_to_pyro(
     parameters: Dict[str, float]
@@ -183,7 +198,7 @@ _MIRA_TO_PYRO = {
     "StandardNormal1": mira_normal_to_pyro,
     "Normal1": mira_normal_to_pyro,
     "Normal2": mira_normal_to_pyro,
-    "Normal3": mira_normal_to_pyro, 
+    "Normal3": mira_normal_to_pyro,
     "LogNormal1": mira_lognormal_to_pyro,
     "LogNormal2": mira_lognormal_to_pyro,
     "Bernoulli1": mira_bernoulli_to_pyro,
@@ -209,7 +224,7 @@ _MIRA_TO_PYRO = {
     "StudentT2": mira_studentt_to_pyro,
     "StudentT3": mira_studentt_to_pyro,
     "Weibull1": mira_weibull_to_pyro,
-    "Weibull2": mira_weibull_to_pyro
+    "Weibull2": mira_weibull_to_pyro,
 }
 
 
