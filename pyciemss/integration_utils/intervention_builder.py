87b70ab96b150e1b08b21ad151f8a9e3287744e9
Anirban Chaudhuri <75496534+anirban-chaudhuri@users.noreply.github.com>
2024-12-05 10:59:29 -0500

    Added docstring to intervention templates (#633)
    * Added docstring to intervention templates
    
    * Lint

--- pyciemss/integration_utils/intervention_builder.py
+++ pyciemss/integration_utils/intervention_builder.py
@@ -9,14 +9,29 @@ def param_value_objective(
     start_time: List[torch.Tensor],
     param_value: List[Intervention] = [None],
 ) -> Callable[[torch.Tensor], Dict[float, Dict[str, Intervention]]]:
+    """
+    Create static parameter intervention template for optimizing parameter values.
+
+    Args:
+        param_name (List[str]): A list of parameter names to associate with interventions.
+        start_time (List[torch.Tensor]): A list of start times for the interventions.
+        param_value (List[Intervention]): A list of parameter values (or callable functions
+            that return the values). Default is a list of None values, in which case a simple identity
+            function is used.
+
+    Returns:
+        intervention_generator (Callable[[torch.Tensor], Dict[float, Dict[str, Intervention]]]): A function that,
+        when given an input tensor `x` for parameter values, returns a dictionary of interventions at teh specified
+        times.
+    """
     param_size = len(param_name)
     if len(param_value) < param_size and param_value[0] is None:
         param_value = [None for _ in param_name]
     for count in range(param_size):
         if param_value[count] is None:
             if not callable(param_value[count]):
+                # Note that param_value needs to be Callable
                 param_value[count] = lambda y: torch.tensor(y)
-    # Note that param_value needs to be Callable
 
     def intervention_generator(
         x: torch.Tensor,
@@ -55,6 +70,17 @@ def start_time_objective(
     param_name: List[str],
     param_value: List[torch.Tensor],
 ) -> Callable[[torch.Tensor], Dict[float, Dict[str, Intervention]]]:
+    """
+    Create static parameter intervention template for optimizing start times.
+
+    Args:
+        param_name (List[str]): A list of parameter names to associate with interventions.
+        param_value (List[torch.Tensor]): A list of parameter values (as tensors) corresponding to the parameters.
+
+    Returns:
+        intervention_generator (Callable[[torch.Tensor], Dict[float, Dict[str, Intervention]]]): A function that,
+        when given an input tensor `x` for start times, returns a dictionary of interventions at those times.
+    """
     param_size = len(param_name)
     # Note: code below will only work for tensors and not callable functions
     param_value = [torch.atleast_1d(y) for y in param_value]
@@ -90,6 +116,19 @@ def start_time_param_value_objective(
     param_name: List[str],
     param_value: List[Intervention] = [None],
 ) -> Callable[[torch.Tensor], Dict[float, Dict[str, Intervention]]]:
+    """
+    Create static parameter intervention template for optimizing start times and parameter values at the same time.
+
+    Args:
+        param_name (List[str]): A list of parameter names to associate with interventions.
+        param_value (List[Intervention]): A list of parameter values (or callable functions
+            that return the values). Default is a list of None values, in which case a simple identity
+            function is used.
+
+    Returns:
+        intervention_generator (Callable[[torch.Tensor], Dict[float, Dict[str, Intervention]]]): A function that,
+        when given an input tensor `x`, returns a dictionary of interventions at specific times.
+    """
     param_size = len(param_name)
     if len(param_value) < param_size and param_value[0] is None:
         param_value = [None for _ in param_name]
@@ -137,6 +176,19 @@ def intervention_func_combinator(
     ],
     intervention_func_lengths: List[int],
 ) -> Callable[[torch.Tensor], Dict[float, Dict[str, Intervention]]]:
+    """
+    Combine multiple intervention functions into a single function.
+
+    Args:
+        intervention_funcs (List[Callable[[torch.Tensor], Dict[float, Dict[str, Intervention]]]]):
+            A list of intervention functions to be combined.
+        intervention_func_lengths (List[int]): A list of lengths corresponding to the input size expected
+            by each intervention function.
+
+    Returns:
+        intervention_generator (Callable[[torch.Tensor], Dict[float, Dict[str, Intervention]]]): A combined
+        intervention function that calls each individual function with the appropriate portion of the input tensor.
+    """
     assert len(intervention_funcs) == len(intervention_func_lengths), (
         f"Size mismatch between number of intervention functions ('{len(intervention_funcs)}')"
         f"and number of intervention function lengths ('{len(intervention_func_lengths)}') "
@@ -167,6 +219,17 @@ def intervention_func_combinator(
 def combine_static_parameter_interventions(
     interventions: List[Dict[float, Dict[str, Intervention]]]
 ) -> Dict[float, Dict[str, Intervention]]:
+    """
+    Combine a list of static parameter interventions into one.
+
+    Args:
+        interventions (List[Dict[float, Dict[str, Intervention]]]): A list of static parameter interventions to be
+        combined.
+
+    Returns:
+        static_parameter_interventions (Dict[float, Dict[str, Intervention]]): A single dictionary containing all
+        combined interventions.
+    """
     static_parameter_interventions: Dict[float, Dict[str, Intervention]] = {}
     for intervention in interventions:
         for key, value in intervention.items():
